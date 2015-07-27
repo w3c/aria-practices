81c97327366b104145c7d73f289fca7efb4ec322
Michiel Bijl <m.bijl@lukkien.com>
2015-07-27 23:52:05 +0200

    Add div button, add comments to JavaScript

--- examples/button/js/button.js
+++ examples/button/js/button.js
@@ -1,21 +1,29 @@
 function init() {
+  // Create variables for the various buttons
   var printButton = document.getElementById('print');
+  var alertButton =  document.getElementById('button');
   var toggleButton = document.getElementById('toggle');
-  
+
+  // Add event listeners to the various buttons
   printButton.addEventListener('click', printButtonEventHandler);
   printButton.addEventListener('keydown', printButtonEventHandler);
-  
+
+  alertButton.addEventListener('click', alertButtonEventHandler);
+  alertButton.addEventListener('keydown', alertButtonEventHandler);
+
   toggleButton.addEventListener('click', toggleButtonEventHandler);
   toggleButton.addEventListener('keydown', toggleButtonEventHandler);
 }
 
 function printButtonEventHandler(event) {
   var type = event.type;
-  
+
+  // Grab the keydown and click events
   if (type === 'keydown') {
+    // If either enter or space is pressed, execute the funtion
     if (event.keyCode === 13 || event.keyCode === 32) {
       window.print();
-      
+
       event.preventDefault();
     }
   } else if (type === 'click') {
@@ -23,13 +31,32 @@ function printButtonEventHandler(event) {
   }
 }
 
+function alertButtonEventHandler(event) {
+  var type = event.type;
+  var message = 'Hej, hello, ciao, こんにちは';
+
+  // Grab the keydown and click events
+  if (type === 'keydown') {
+    // If either enter or space is pressed, execute the funtion
+    if (event.keyCode === 13 || event.keyCode === 32) {
+      alert(message);
+
+      event.preventDefault();
+    }
+  } else if (type === 'click') {
+    alert(message);
+  }
+}
+
 function toggleButtonEventHandler(event) {
   var type = event.type;
-  
+
+  // Grab the keydown and click events
   if (type === 'keydown') {
+    // If either enter or space is pressed, execute the funtion
     if (event.keyCode === 13 || event.keyCode === 32) {
       toggleButtonState(event);
-      
+
       event.preventDefault();
     }
   } else if (type === 'click') {
@@ -41,11 +68,13 @@ function toggleButtonState(event) {
   var button = event.target;
   var currentState = button.getAttribute('aria-pressed');
   var newState = 'true';
-  
+
+  // If aria-pressed is set to true, set newState to false
   if (currentState === 'true') {
     newState = 'false';
   }
-  
+
+  // Set the new aria-pressed state on the button
   button.setAttribute('aria-pressed', newState);
 }
 
