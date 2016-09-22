48f50468290f4e4e36f97ab076b78f0fbba2169a
Jesse Renee Beach <splendidnoise@gmail.com>
2016-09-22 18:07:41 +0100

    Fixed radio

--- examples/radio/js/radio.js
+++ examples/radio/js/radio.js
@@ -1,10 +1,10 @@
 var KEYCODE = {
-    DOWN: 40,
-    LEFT: 37,
-    RIGHT: 39,
-    SPACE: 32,
-    UP: 38
-}
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39,
+  SPACE: 32,
+  UP: 38
+};
 
 window.addEventListener('load', function () {
 
@@ -13,7 +13,7 @@ window.addEventListener('load', function () {
   for (var i = 0; i < radiobuttons.length; i++) {
     var rb = radiobuttons[i];
 
-    console.log(rb.tagName + ' ' + rb.id)
+    console.log(rb.tagName + ' ' + rb.id);
 
     rb.addEventListener('click', clickRadioGroup);
     rb.addEventListener('keydown', keyDownRadioGroup);
@@ -35,7 +35,7 @@ function firstRadioButton (node) {
 
   var first = node.parentNode.firstChild;
 
-  while(first) {
+  while (first) {
     if (first.nodeType === Node.ELEMENT_NODE) {
       if (first.getAttribute('role') === 'radio') return first;
     }
@@ -57,7 +57,7 @@ function lastRadioButton (node) {
 
   var last = node.parentNode.lastChild;
 
-  while(last) {
+  while (last) {
     if (last.nodeType === Node.ELEMENT_NODE) {
       if (last.getAttribute('role') === 'radio') return last;
     }
@@ -79,7 +79,7 @@ function nextRadioButton (node) {
 
   var next = node.nextSibling;
 
-  while(next) {
+  while (next) {
     if (next.nodeType === Node.ELEMENT_NODE) {
       if (next.getAttribute('role') === 'radio') return next;
     }
@@ -101,7 +101,7 @@ function previousRadioButton (node) {
 
   var prev = node.previousSibling;
 
-  while(prev) {
+  while (prev) {
     if (prev.nodeType === Node.ELEMENT_NODE) {
       if (prev.getAttribute('role') === 'radio') return prev;
     }
@@ -123,7 +123,7 @@ function getImage (node) {
 
   var child = node.firstChild;
 
-  while(child) {
+  while (child) {
     if (child.nodeType === Node.ELEMENT_NODE) {
       if (child.tagName === 'IMG') return child;
     }
@@ -146,12 +146,12 @@ function setRadioButton (node, state) {
   var image = getImage(node);
 
   if (state == 'true') {
-    node.setAttribute('aria-checked', 'true')
+    node.setAttribute('aria-checked', 'true');
     node.tabIndex = 0;
-    node.focus()
+    node.focus();
   }
   else {
-    node.setAttribute('aria-checked', 'false')
+    node.setAttribute('aria-checked', 'false');
     node.tabIndex = -1;
   }
 }
@@ -198,20 +198,24 @@ function keyDownRadioGroup (event) {
   var type = event.type;
   var next = false;
 
-  if(type === 'keydown') {
+  if (type === 'keydown') {
     var node = event.currentTarget;
 
     switch (event.keyCode) {
       case KEYCODE.DOWN:
       case KEYCODE.RIGHT:
         var next = nextRadioButton(node);
-        if (!next) next = firstRadioButton(node); //if node is the last node, node cycles to first.
+        if (!next) {
+          next = firstRadioButton(node); // if node is the last node, node cycles to first.
+        }
         break;
 
       case KEYCODE.UP:
       case KEYCODE.LEFT:
         next = previousRadioButton(node);
-        if (!next) next = lastRadioButton(node); //if node is the last node, node cycles to first.
+        if (!next) {
+          next = lastRadioButton(node); // if node is the last node, node cycles to first.
+        }
         break;
 
       case KEYCODE.SPACE:
@@ -256,5 +260,5 @@ function focusRadioButton (event) {
 */
 
 function blurRadioButton (event) {
-   event.currentTarget.className = event.currentTarget.className.replace(' focus','');
+  event.currentTarget.className = event.currentTarget.className.replace(' focus', '');
 }
