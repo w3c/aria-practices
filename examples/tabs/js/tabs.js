bcc7decc67a378995396b9a6f809b709d0350428
Michiel Bijl <michiel@agosto.nl>
2016-09-25 03:16:51 +0100

    Rework large part of tabs widget code example

--- examples/tabs/js/tabs.js
+++ examples/tabs/js/tabs.js
@@ -2,6 +2,7 @@
   var tablist = document.querySelectorAll('[role="tablist"]')[0];
   var tabs = document.querySelectorAll('[role="tab"]');
   var panels = document.querySelectorAll('[role="tabpanel"]');
+  var delay = determineDelay();
 
   // For easy reference
   var keys = {
@@ -28,61 +29,71 @@
   };
 
   function addListeners (index) {
-    tabs[index].addEventListener('click', activateTab);
-    tabs[index].addEventListener('keyup', keyEventListener);
-    tabs[index].addEventListener('focus', focusTab);
+    tabs[index].addEventListener('click', clickEventListener);
+    tabs[index].addEventListener('keydown', keydownEventListener);
+    tabs[index].addEventListener('keyup', keyupEventListener);
+
+    // Build an array with all tabs (<button>s) in it
     tabs[index].index = index;
   };
 
-  // Activate a tab panel after 300 milliseconds
-  function focusTab (event) {
-    setTimeout(activateTab, 300, event);
+  // When a tab is clicked, activateTab is fired to activate it
+  function clickEventListener (event) {
+    var tab = event.target;
+    activateTab(tab, false);
   };
 
-  // Activates any given tab panel
-  function activateTab (event) {
-    deactivateTabs();
-
-    event.target.removeAttribute('tabindex');
-    event.target.setAttribute('aria-selected', 'true');
-
-    var controls = event.target.getAttribute('aria-controls');
-    document.getElementById(controls).removeAttribute('hidden');
-  };
-
-  // Handle key pressed on tabs
-  function keyEventListener (event) {
+  // Handle keydown on tabs
+  function keydownEventListener (event) {
     var key = event.keyCode;
 
     switch (key) {
       case keys.end:
         event.preventDefault();
-        focusLastTab();
+        // Activate last tab
+        activateTab(tabs[tabs.length - 1]);
         break;
       case keys.home:
         event.preventDefault();
-        focusFirstTab();
+        // Activate first tab
+        activateTab(tabs[0]);
         break;
-      case keys.left:
-      case keys.right:
+
+      // Up and down are in keydown
+      // because we need to prevent page scroll >:)
       case keys.up:
       case keys.down:
         determineOrientation(event);
         break;
+    };
+  };
+
+  // Handle keyup on tabs
+  function keyupEventListener (event) {
+    var key = event.keyCode;
+
+    switch (key) {
+      case keys.left:
+      case keys.right:
+        determineOrientation(event);
+        break;
       case keys.delete:
-        detectDeletable(event);
+        determineDeletable(event);
         break;
     };
   };
 
+  // When a tablist’s aria-orientation is set to vertical,
+  // only up and down arrow should function.
+  // In all other cases only left and right arrow function.
   function determineOrientation (event) {
-    var target = event.target;
     var key = event.keyCode;
     var vertical = tablist.getAttribute('aria-orientation') == 'vertical';
     var proceed = false;
 
     if (vertical) {
       if (key === keys.up || key === keys.down) {
+        event.preventDefault();
         proceed = true;
       };
     }
@@ -93,14 +104,18 @@
     };
 
     if (proceed) {
-      switchTab(event);
+      switchTabOnArrowPress(event);
     };
   };
 
   // Either focus the next, previous, first, or last tab
   // depening on key pressed
-  function switchTab (event) {
+  function switchTabOnArrowPress (event) {
     var pressed = event.keyCode;
+
+    for (x = 0; x < tabs.length; x++) {
+      tabs[x].addEventListener('focus', focusEventHandler);
+    };
 
     if (direction[pressed]) {
       var target = event.target;
@@ -118,11 +133,35 @@
     };
   };
 
+  // Activates any given tab panel
+  function activateTab (tab, setFocus = true) {
+    // Deactivate all other tabs
+    deactivateTabs();
+
+    // Remove tabindex attribute
+    tab.removeAttribute('tabindex');
+
+    // Set the tab as selected
+    tab.setAttribute('aria-selected', 'true');
+
+    // Get the value of aria-controls (which is an ID)
+    var controls = tab.getAttribute('aria-controls');
+
+    // Remove hidden attribute from tab panel to make it visible
+    document.getElementById(controls).removeAttribute('hidden');
+
+    // Set focus when required
+    if (setFocus) {
+      tab.focus();
+    };
+  };
+
   // Deactivate all tabs and tab panels
   function deactivateTabs () {
     for (t = 0; t < tabs.length; t++) {
       tabs[t].setAttribute('tabindex', '-1');
       tabs[t].setAttribute('aria-selected', 'false');
+      tabs[t].removeEventListener('focus');
     };
 
     for (p = 0; p < panels.length; p++) {
@@ -141,32 +180,58 @@
   };
 
   // Detect if a tab is deletable
-  function detectDeletable (event) {
+  function determineDeletable (event) {
     target = event.target;
     if (target.getAttribute('data-deletable') !== undefined) {
+      // Delete target tab
       deleteTab(event, target);
-      focusFirstTab();
+
+      // Activate the first tab
+      activateTab(tabs[0]);
     };
   };
 
   // Deletes a tab and its panel
   function deleteTab (event) {
-    if (!target) {
-      target = event.target;
-    };
-    panel = document.getElementById(target.getAttribute('aria-controls'));
+    var target = event.target;
+    var panel = document.getElementById(target.getAttribute('aria-controls'));
 
     target.remove();
     panel.remove();
   };
 
-  // Focus panel
-  function focusPanel (event) {
-    var panelID = event.target.getAttribute('aria-controls');
+  // Determine whether there should be a delay
+  // when user navigates with the arrow keys
+  function determineDelay () {
+    var hasDelay = tablist.hasAttribute('data-delay');
+    var delay = 0;
 
-    var panel = document.getElementById(panelID);
+    if (hasDelay) {
+      var delayValue = tablist.getAttribute('data-delay');
+      if (delayValue) {
+        delay = delayValue;
+      } else {
+        // If no value is specified, default to 300ms
+        delay = 300;
+      };
+    };
 
-    panel.setAttribute('tabindex', -1);
-    panel.focus();
+    return delay;
+  };
+
+  //
+  function focusEventHandler (event) {
+    var target = event.target;
+
+    setTimeout(checkTabFocus, delay, target);
+  };
+
+  // Only activate tab on focus if it still has focus after the delay
+  function checkTabFocus(target) {
+    focused = document.activeElement;
+
+    if (target === focused) {
+      activateTab(target, false);
+    };
   };
 })();
