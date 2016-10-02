98fd726d073fadafedd046a2beff10f928684862
JaEun Jemma Ku <a11ydoer@gmail.com>
2016-10-02 13:27:05 -0500

    copyright info change for w3c aria example

--- examples/menubar/menubar-1/js/MenuItemLinks.js
+++ examples/menubar/menubar-1/js/MenuItemLinks.js
@@ -1,23 +1,14 @@
 /*
-*   Copyright 2016 University of Illinois
+/*
+*   This content is licensed according to the W3C Software License at 
+*   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
 *
-*   Licensed under the Apache License, Version 2.0 (the "License");
-*   you may not use this file except in compliance with the License.
-*   You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-*   Unless required by applicable law or agreed to in writing, software
-*   distributed under the License is distributed on an "AS IS" BASIS,
-*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*   See the License for the specific language governing permissions and
-*   limitations under the License.
 *
 *   File:   MenuItem.js
 *
 *   Desc:   Popup Menu Menuitem widget that implements ARIA Authoring Practices
 *
-*   Author: Jon Gunderson, Ku Ja Eun and Nicholas Hoyt
+*   Author: Jon Gunderson, JaEun Jemma Ku and Nicholas Hoyt
 */
 
 /*
--- examples/menubar/menubar-2/js/TextStyling.js
+++ examples/menubar/menubar-2/js/TextStyling.js
@@ -1,23 +1,13 @@
 /*
-*   Copyright 2016 University of Illinois
+*   This content is licensed according to the W3C Software License at 
+*   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
 *
-*   Licensed under the Apache License, Version 2.0 (the "License");
-*   you may not use this file except in compliance with the License.
-*   You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-*   Unless required by applicable law or agreed to in writing, software
-*   distributed under the License is distributed on an "AS IS" BASIS,
-*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*   See the License for the specific language governing permissions and
-*   limitations under the License.
 *
 *   File:   TextStyling.js
 *
 *   Desc:   Styling functions for changing the style of an item
 *
-*   Author: Jon Gunderson and Ku Ja Eun
+*   Author: Jon Gunderson and JaEun Jemma Ku
 */
 
 /*
