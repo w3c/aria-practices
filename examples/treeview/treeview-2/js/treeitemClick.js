ecc6bccc2c14cae62d84fb1dcb1315c0af7f19e5
Jon Gunderson <jongund@illinois.edu>
2016-11-30 21:14:54 -0600

    update review examples

--- examples/treeview/treeview-2/js/treeitemClick.js
+++ examples/treeview/treeview-2/js/treeitemClick.js
@@ -1,23 +1,18 @@
 /*
- * Copyright 2011-2016 University of Illinois
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+*   This content is licensed according to the W3C Software License at
+*   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+*
+*   File:   Treeitem.js
+*
+*   Desc:   Setup click events for Tree widget examples
+
+*   Author: Jon Gunderson, Ku Ja Eun and Nicholas Hoyt
+*/
 
 /**
  * ARIA Treeview example
  * @function onload
- * @desc  after page has loaded initializ all treeitems based on the role=treeitem
+ * @desc  after page has loaded initialize all treeitems based on the role=treeitem
  */
 
 window.addEventListener('load', function () {
