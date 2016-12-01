7c4c17d954e349539456004bef4ac888bb585b3e
Tatiana <tatermelon@gmail.com>
2016-12-01 13:34:08 -0800

    merged conflicts

--- examples/toolbar/js/main.js
+++ examples/toolbar/js/main.js
@@ -1,9 +1,5 @@
 /**
-<<<<<<< 8e2e23c8be8498cd4c828dcd9ce6886266d68f6c
  * ARIA Toolbar Examples
-=======
- * ARIA Grid Examples
->>>>>>> Setup toolbar example
  * @function onload
  * @desc Initialize the toolbar example once the page has loaded
  */
