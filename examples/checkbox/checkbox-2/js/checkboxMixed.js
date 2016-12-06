3cdb5b527fcd67462b2b37cdda3177f7ce5200f7
Jon Gunderson <jongund@illinois.edu>
2016-12-06 13:21:55 -0600

    updated checkbox

--- examples/checkbox/checkbox-2/js/checkboxMixed.js
+++ examples/checkbox/checkbox-2/js/checkboxMixed.js
@@ -19,6 +19,8 @@ var CheckboxMixed = function (domNode) {
 
   this.domNode = domNode;
 
+  this.controlledCheckboxes = [];
+
   this.keyCode = Object.freeze({
     'RETURN'   : 13,
     'SPACE'    : 32,
@@ -28,8 +30,13 @@ var CheckboxMixed = function (domNode) {
 CheckboxMixed.prototype.init = function () {
   this.domNode.tabIndex = 0;
 
-  if (!this.domNode.getAttribute('aria-checked')) {
-    this.domNode.setAttribute('aria-checked', 'false');
+  var ids = this.domNode.getAttribute('aria-controls').split(' ');
+
+  for (var i = 0; i < ids.length; i++) {
+    var node = document.getElementById(ids[i]);
+    var ccb = new ControlledCheckbox(node, this);
+    ccb.init();
+    this.controlledCheckboxes.push(ccb);
   }
 
   this.domNode.addEventListener('keydown',    this.handleKeydown.bind(this));
@@ -37,18 +44,80 @@ CheckboxMixed.prototype.init = function () {
   this.domNode.addEventListener('focus',      this.handleFocus.bind(this));
   this.domNode.addEventListener('blur',       this.handleBlur.bind(this));
 
+  this.updateCheckboxMixed();
+
+};
+
+CheckboxMixed.prototype.updateCheckboxMixed = function() {
+
+  var count = 0;
+
+  for (var i = 0; i < this.controlledCheckboxes.length; i++) {
+    if (this.controlledCheckboxes[i].isChecked()) {
+      count++;
+    }
+  }
+
+  if (count === 0) {
+    this.domNode.setAttribute('aria-checked', 'false');
+  }
+  else {
+    if (count === this.controlledCheckboxes.length) {
+      this.domNode.setAttribute('aria-checked', 'true');
+    }
+    else {
+      this.domNode.setAttribute('aria-checked', 'mixed');
+    }
+  }
+};
+
+CheckboxMixed.prototype.anyLastChecked = function() {
+
+  var count = 0;
+
+  for (var i = 0; i < this.controlledCheckboxes.length; i++) {
+    if (this.controlledCheckboxes[i].lastState) {
+      count++;
+    }
+  }
+
+  return count > 0;
+
+};
+
+CheckboxMixed.prototype.setControlledCheckboxes = function (value) {
+
+  for (var i = 0; i < this.controlledCheckboxes.length; i++) {
+    this.controlledCheckboxes[i].setChecked(value);
+  }
+
+  this.updateCheckboxMixed();
+
 };
 
 CheckboxMixed.prototype.toggleCheckboxMixed = function() {
 
-  if (this.domNode.getAttribute('aria-checked') === 'true') {
-    this.domNode.setAttribute('aria-checked', 'false');
+  var state = this.domNode.getAttribute('aria-checked');
+
+  if (state === 'false') {
+    if (this.anyLastChecked()) {
+      this.setControlledCheckboxes('last');
+    }
+    else {
+      this.setControlledCheckboxes('true');
+    }
   }
   else {
-    this.domNode.setAttribute('aria-checked', 'true');
+    if (state === 'mixed') {
+      this.setControlledCheckboxes('true');
+    }
+    else {
+      this.setControlledCheckboxes('false');        
+    }
   }
 
-}
+  this.updateCheckboxMixed();
+};
 
 /* EVENT HANDLERS */
 
