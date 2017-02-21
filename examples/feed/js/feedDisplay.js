8e219e2be8314df76a26e18d0a921c562594bffb
Tatiana <tatermelon@gmail.com>
2017-02-21 10:31:18 -0800

    Styled restaruant items and added location info

--- examples/feed/js/feedDisplay.js
+++ examples/feed/js/feedDisplay.js
@@ -8,70 +8,90 @@ aria.RestaurantData = [
     name: "Tito's Tacos",
     rating: 5,
     type: "Mexican, Tacos",
-    location: "San Dimas, CA",
+    street: '123 Blueberry Ln',
+    citystate: "San Dimas, CA",
+    phone: "(111) 111-1111",
     image: "",
   },
   {
     name: "Sakura Sushi",
     rating: 4,
     type: "Japanese, Fish, Sushi",
-    location: "Pomona, CA",
+    street: '123 Blueberry Ln',
+    citystate: "Pomona, CA",
+    phone: "(111) 111-1111",
     image: "",
   },
   {
     name: "Prime Steakhouse",
     rating: 4,
     type: "Steakhouses",
-    location: "Claremont, CA",
+    street: '123 Blueberry Ln',
+    citystate: "Claremont, CA",
+    phone: "(111) 111-1111",
     image: "",
   },
   {
     name: "The Pizza Factory",
     rating: 4,
     type: "Pizza",
-    location: "Pomona, CA",
+    street: '123 Blueberry Ln',
+    citystate: "Pomona, CA",
+    phone: "(111) 111-1111",
     image: "",
   },
   {
     name: "Emperor's Mongolian",
     rating: 5,
     type: "Mongolian, Barbequeue, Buffets",
-    location: "La Verne, CA",
+    street: '123 Blueberry Ln',
+    citystate: "La Verne, CA",
+    phone: "(111) 111-1111",
     image: "",
   },
   {
     name: "Backyard Grill",
     rating: 3,
     type: "American, Burgers",
-    location: "San Dimas, CA",
+    street: '123 Blueberry Ln',
+    citystate: "San Dimas, CA",
+    phone: "(111) 111-1111",
     image: "",
   },
   {
     name: "Taste Kitchen",
     rating: 4,
     type: "American",
-    location: "Claremont, CA",
+    street: '123 Blueberry Ln',
+    citystate: "Claremont, CA",
+    phone: "(111) 111-1111",
     image: "",
   },
   {
     name: "Bon Appetit",
     rating: 4,
     type: "French",
-    location: "La Verne, CA",
+    street: '123 Blueberry Ln',
+    citystate: "La Verne, CA",
+    phone: "(111) 111-1111",
     image: "",
   },
   {
     name: "Sally's Sandwiches",
     rating: 3,
     type: "Sandwiches, American",
-    location: "San Dimas, CA",
+    street: '123 Blueberry Ln',
+    citystate: "San Dimas, CA",
+    phone: "(111) 111-1111",
     image: "",
   },
   {
     name: "The HotPot Spot",
     rating: 5,
     type: "Hot Pot, Japanese",
-    location: "Pomona, CA",
+    street: '123 Blueberry Ln',
+    citystate: "Pomona, CA",
+    phone: "(111) 111-1111",
     image: "",
   },
 ];
@@ -81,6 +101,7 @@ aria.FeedDisplay = function (feedNode, fetchData) {
   this.fetchData = fetchData;
   this.currentPage = 0;
   this.perPage = 10;
+  this.currentIndex = 0;
   this.loadData();
 };
 
@@ -92,13 +113,86 @@ aria.FeedDisplay.prototype.loadData = function () {
   }
 
   Array.prototype.forEach.call(feedData, (function (itemData) {
-    var feedItem = document.createElement('div');
-
-    feedItem.setAttribute('role', 'article');
-    feedItem.innerText = itemData.name;
-
-    this.feedNode.append(feedItem);
+    this.feedNode.appendChild(this.renderItemData(itemData));
   }).bind(this));
+
+  // TODO: update aria-setsize for all elements
+};
+
+aria.FeedDisplay.prototype.renderItemData = function (itemData) {
+  var feedItem = document.createElement('div');
+
+  this.currentIndex++;
+
+  feedItem.setAttribute('role', 'article');
+  feedItem.className = 'restaurant-item';
+  feedItem.setAttribute('aria-labelledby', 'feed-name-' + this.currentIndex);
+  feedItem.setAttribute('aria-posinset', this.currentIndex);
+  feedItem.setAttribute('tabindex', '0');
+
+  var itemDetails = document.createElement('div');
+  itemDetails.className = 'restaurant-details';
+  var itemContent = '';
+
+  if (itemData.image) {
+    itemContent += '<div class="restaurant-image">'
+                   + itemData.image
+                   + '</div>';
+  }
+
+  itemContent += '<div class="restaurant-name" '
+                 + 'id="feed-name-' + this.currentIndex + '">'
+                 + itemData.name
+                 + '</div>';
+
+  if (itemData.rating) {
+    itemContent += '<div class="restaurant-rating">'
+                   + '<img class="restaurant-star-img" '
+                   + 'alt="' + itemData.rating + ' stars" '
+                   + 'src="imgs/rating-' + itemData.rating + '.png" />'
+                   + '</div>';
+  }
+
+  if (itemData.type) {
+    itemContent += '<div class="restaurant-type">'
+                   + itemData.type
+                   + '</div>';
+  }
+
+  itemDetails.innerHTML = itemContent;
+  feedItem.appendChild(itemDetails);
+
+  var locationBlock = document.createElement('div');
+  locationBlock.className = 'location-block';
+  var locationContent = '';
+
+  if (itemData.street) {
+    locationContent += '<div class="restaurant-street">'
+                   + itemData.street
+                   + '</div>';
+  }
+
+  if (itemData.citystate) {
+    locationContent += '<div class="restaurant-citystate">'
+                   + itemData.citystate
+                   + '</div>';
+  }
+
+  if (itemData.phone) {
+    locationContent += '<div class="restaurant-phone">'
+                   + itemData.phone
+                   + '</div>';
+  }
+
+  locationBlock.innerHTML = locationContent;
+  feedItem.appendChild(locationBlock);
+
+  var actions = document.createElement('div');
+  actions.className = 'restaurant-actions';
+  actions.innerHTML = '<button class="bookmark-button">Bookmark</button>';
+  feedItem.appendChild(actions);
+
+  return feedItem;
 };
 
 /**
