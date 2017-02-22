8a0646b55c0404e78bcf0ac9a6718a58bd22888b
Tatiana <tatermelon@gmail.com>
2017-02-21 16:57:26 -0800

    Added feed loading and behaviors

--- examples/feed/js/feed.js
+++ examples/feed/js/feed.js
@@ -1,3 +1,95 @@
 /**
- * Rename this file to the name of the example, e.g., checkbox.js.
+ * @namespace aria
  */
+var aria = aria || {};
+
+// anchorItem item to focus on with control + END
+aria.Feed = function (feedNode, anchorItem) {
+  this.feedNode = feedNode;
+  this.anchorItem = anchorItem;
+  this.feedItems = [];
+  this.focusedIndex = null;
+  this.setupEvents();
+};
+
+aria.Feed.prototype.getFeedNode = function () {
+  return this.feedNode;
+};
+
+aria.Feed.prototype.addItem = function (item) {
+  this.feedItems.push(item);
+};
+
+aria.Feed.prototype.setupEvents = function () {
+  this.feedNode.addEventListener('click', this.handleArticleClicked.bind(this));
+  this.feedNode.addEventListener('keyup', this.handleArticleTabbed.bind(this));
+  this.feedNode.addEventListener('keydown', this.mapKeyShortcut.bind(this));
+  // this.feedNode.addEventListener('focus', );
+
+};
+
+aria.Feed.prototype.handleArticleClicked = function (event) {
+  var clickedItem = event.target;
+
+  if (aria.Utils.matches(clickedItem, '[role="article"]')) {
+    this.focusItem(clickedItem);
+  }
+};
+
+aria.Feed.prototype.handleArticleTabbed = function (event) {
+  var key = event.which || event.keyCode;
+  var tabbedItem = event.target;
+
+  if (key !== aria.KeyCode.TAB) {
+    return;
+  }
+
+  if (aria.Utils.matches(tabbedItem, '[role="article"]')) {
+    this.focusItem(tabbedItem);
+  }
+};
+
+aria.Feed.prototype.focusItem = function (item) {
+  if (!item || !item.focus) {
+    return;
+  }
+
+  item.focus();
+  this.focusedIndex = item.getAttribute('aria-posinset');
+};
+
+aria.Feed.prototype.mapKeyShortcut = function (event) {
+  var key = event.which || event.keyCode;
+
+  switch (key) {
+    case aria.KeyCode.PAGE_UP:
+      event.preventDefault();
+      // Move up focus
+      if (this.focusedIndex > 1) {
+        // Need to increment by 2 because focusIndex is 1-indexed
+        this.focusItem(this.feedItems[this.focusedIndex - 2]);
+      }
+      break;
+    case aria.KeyCode.PAGE_DOWN:
+      event.preventDefault();
+      // Move down focus
+      if (this.feedItems.length >= this.focusedIndex) {
+        // Do not need to increment focusIndex because it is 1-indexed
+        this.focusItem(this.feedItems[this.focusedIndex]);
+      }
+      break;
+    case aria.KeyCode.HOME:
+      if (event.ctrlKey && this.feedItems.length > 0) {
+        event.preventDefault();
+        this.focusItem(this.feedItems[0]);
+      }
+      break;
+    case aria.KeyCode.END:
+      if (event.ctrlKey) {
+        event.preventDefault();
+        this.anchorItem.focus();
+      }
+      break;
+  }
+
+};
--- examples/feed/js/feedDisplay.js
+++ examples/feed/js/feedDisplay.js
@@ -96,43 +96,89 @@ aria.RestaurantData = [
   },
 ];
 
-aria.FeedDisplay = function (feedNode, fetchData) {
-  this.feedNode = feedNode;
+aria.FeedDisplay = function (feed, fetchData) {
+  this.feed = feed;
+  this.feedNode = feed.getFeedNode();
   this.fetchData = fetchData;
   this.currentPage = 0;
   this.perPage = 10;
-  this.currentIndex = 0;
+  this.feedSize = 0;
+  this.feedItems = [];
+  this.loading = false;
+  this.loadingDelay = 200; // in milliseconds
+
+  this.lastChecked = Date.now();
   this.loadData();
+  this.setupEvents();
+};
+
+aria.FeedDisplay.prototype.setDelay = function (delay) {
+  this.loadingDelay = delay;
 };
 
 aria.FeedDisplay.prototype.loadData = function () {
   var feedData = this.fetchData(this.currentPage, this.perPage);
 
-  if (!this.feedNode) {
+  if (!this.feedNode || this.loading) {
     return;
   }
 
-  Array.prototype.forEach.call(feedData, (function (itemData) {
-    this.feedNode.appendChild(this.renderItemData(itemData));
-  }).bind(this));
+  this.feedNode.setAttribute('aria-busy', 'true');
+  this.loading = true;
 
-  // TODO: update aria-setsize for all elements
+  var loadingItems = [];
+
+  Array.prototype.forEach.call(feedData, function (itemData) {
+    var newFeedItem = this.renderItemData(itemData);
+    loadingItems.push(newFeedItem);
+  }, this);
+
+  this.delayRender(
+    loadingItems,
+    function () {
+      this.feedNode.removeAttribute('aria-busy');
+      this.loading = false;
+      this.checkLoadMore();
+    }.bind(this)
+  );
+};
+
+aria.FeedDisplay.prototype.delayRender = function (items, onRenderDone) {
+  if (!items || !items.length) {
+    onRenderDone();
+    return;
+  }
+
+  var newFeedItem = items.shift();
+  this.feedNode.appendChild(newFeedItem);
+  this.feedItems.push(newFeedItem);
+  this.feed.addItem(newFeedItem);
+
+  this.feedItems.forEach(function (feedItem) {
+    feedItem.setAttribute('aria-setsize', this.feedItems.length);
+  }, this);
+
+  setTimeout((function () {
+    this.delayRender(items, onRenderDone);
+  }).bind(this), this.loadingDelay);
 };
 
 aria.FeedDisplay.prototype.renderItemData = function (itemData) {
   var feedItem = document.createElement('div');
 
-  this.currentIndex++;
+  this.feedSize++;
 
   feedItem.setAttribute('role', 'article');
   feedItem.className = 'restaurant-item';
-  feedItem.setAttribute('aria-labelledby', 'feed-name-' + this.currentIndex);
-  feedItem.setAttribute('aria-posinset', this.currentIndex);
+  feedItem.setAttribute('aria-posinset', this.feedSize);
   feedItem.setAttribute('tabindex', '0');
 
   var itemDetails = document.createElement('div');
   itemDetails.className = 'restaurant-details';
   var itemContent = '';
+  var describedbyIDs = [];
+  var restaurantID = 'restaurant-name-' + this.feedSize;
+  feedItem.setAttribute('aria-labelledby', restaurantID);
 
   if (itemData.image) {
     itemContent += '<div class="restaurant-image">'
@@ -141,30 +187,37 @@ aria.FeedDisplay.prototype.renderItemData = function (itemData) {
   }
 
   itemContent += '<div class="restaurant-name" '
-                 + 'id="feed-name-' + this.currentIndex + '">'
+                 + 'id="' + restaurantID + '">'
                  + itemData.name
                  + '</div>';
 
   if (itemData.rating) {
-    itemContent += '<div class="restaurant-rating">'
+    var ratingID = 'restaurant-rating-' + this.feedSize;
+    itemContent += '<div class="restaurant-rating" id="' + ratingID + '">'
                    + '<img class="restaurant-star-img" '
                    + 'alt="' + itemData.rating + ' stars" '
                    + 'src="imgs/rating-' + itemData.rating + '.png" />'
                    + '</div>';
+    describedbyIDs.push(ratingID);
   }
 
   if (itemData.type) {
-    itemContent += '<div class="restaurant-type">'
+    var typeID = 'restaurant-type-' + this.feedSize;
+    itemContent += '<div class="restaurant-type" id="' + typeID + '">'
                    + itemData.type
                    + '</div>';
+    describedbyIDs.push(typeID);
   }
 
   itemDetails.innerHTML = itemContent;
   feedItem.appendChild(itemDetails);
 
   var locationBlock = document.createElement('div');
+  var locationID = 'restaurant-location-' + this.feedSize;
+  locationBlock.setAttribute('id', locationID);
   locationBlock.className = 'location-block';
   var locationContent = '';
+  describedbyIDs.push(locationID);
 
   if (itemData.street) {
     locationContent += '<div class="restaurant-street">'
@@ -187,6 +240,8 @@ aria.FeedDisplay.prototype.renderItemData = function (itemData) {
   locationBlock.innerHTML = locationContent;
   feedItem.appendChild(locationBlock);
 
+  feedItem.setAttribute('aria-describedby', describedbyIDs.join(' '));
+
   var actions = document.createElement('div');
   actions.className = 'restaurant-actions';
   actions.innerHTML = '<button class="bookmark-button">Bookmark</button>';
@@ -195,14 +250,28 @@ aria.FeedDisplay.prototype.renderItemData = function (itemData) {
   return feedItem;
 };
 
-/**
- * ARIA Feed Display
- * @function onload
- * @desc Initialize the feed once the page has loaded
- */
-window.addEventListener('load', function () {
-  var feedNode = document.getElementById('restaurant-feed');
-  var restaurantFeed = new aria.FeedDisplay(feedNode, function () {
-    return aria.RestaurantData;
-  });
-});
+aria.FeedDisplay.prototype.setupEvents = function () {
+  window.addEventListener('scroll', this.handleScroll.bind(this));
+};
+
+aria.FeedDisplay.prototype.handleScroll = function () {
+  var now = Date.now();
+
+  if ((this.lastChecked + 100 - now) < 0) {
+    this.checkLoadMore();
+    this.lastChecked = now;
+  }
+};
+
+aria.FeedDisplay.prototype.checkLoadMore = function () {
+  if (!this.feedItems || !this.feedItems.length) {
+    return;
+  }
+
+  var lastFeedItem = this.feedItems[this.feedItems.length - 1];
+  var scrollBottom = document.body.scrollTop + window.innerHeight;
+
+  if (scrollBottom >= (lastFeedItem.offsetTop - 300)) {
+    this.loadData();
+  }
+};
