2793facd1b4f27f64fef5dc8f74fcffb22daefcf
Gerard K. Cohen <gerardkcohen@gmail.com>
2017-05-20 17:44:18 -0700

    refactor/ simplify JS and CSS

---
+++ examples/accordion/js/accordion.js
@@ -0,0 +1,98 @@
+Array.from(document.querySelectorAll('.Accordion')).forEach(function (accordion) {
+
+  // Allow for each toggle to both open and close individually
+  const allowToggle = accordion.hasAttribute('data-allow-toggle');
+  // Allow for multiple accordion sections to be expanded at the same time
+  const allowMultiple = accordion.hasAttribute('data-allow-multiple');
+
+  // Create the array of toggle elements for the accordion group
+  const triggers = Array.from(accordion.querySelectorAll('.Accordion-trigger'));
+  const panels = Array.from(accordion.querySelectorAll('.Accordion-panel'));
+
+  accordion.addEventListener('click', function (event) {
+    const target = event.target;
+
+    if (target.classList.contains('Accordion-trigger')) {
+      // Check if the current toggle is expanded.
+      const isExpanded = target.getAttribute('aria-expanded') == 'true';
+
+      if (!allowMultiple) {
+        // Close all previously open accordion toggles
+        triggers.forEach(function (trigger) {
+          if (trigger.getAttribute('aria-expanded') == 'true') {
+            // Hide all accordion sections, using aria-controls to specify the desired section
+            document.getElementById(trigger.getAttribute('aria-controls')).setAttribute('hidden', '');
+            // Set the expanded state on the triggering element
+            trigger.setAttribute('aria-expanded', 'false');
+          }
+        });
+      }
+
+      if (allowToggle && isExpanded) {
+        // Close the activated accordion if allowToggle=true, using aria-controls to specify the desired section
+        document.getElementById(target.getAttribute('aria-controls')).setAttribute('hidden', '');
+        // Set the expanded state on the triggering element
+        target.setAttribute('aria-expanded', 'false');
+      }
+      else if (!allowToggle && !isExpanded) {
+        // Otherwise open the activated accordion, using aria-controls to specify the desired section
+        document.getElementById(target.getAttribute('aria-controls')).removeAttribute('hidden');
+        // Set the expanded state on the triggering element
+        target.setAttribute('aria-expanded', 'true');
+      }
+
+      event.preventDefault();
+    }
+  });
+
+  // Bind keyboard behaviors on the main accordion container
+  accordion.addEventListener('keydown', function (event) {
+    const target = event.target;
+    const key = event.which.toString();
+    // 33 = Page Up, 34 = Page Down
+    const ctrlModifier = (event.ctrlKey && key.match(/33|34/));
+
+    // Is this coming from an accordion header?
+    if (target.classList.contains('Accordion-trigger')) {
+      // Up/ Down arrow and Control + Page Up/ Page Down keyboard operations
+      // 38 = Up, 40 = Down
+      if (key.match(/38|40/) || ctrlModifier) {
+        const index = triggers.indexOf(target);
+        const direction = (key.match(/34|40/)) ? 1 : -1;
+        const length = triggers.length;
+        const newIndex = (index + length + direction) % length;
+
+        triggers[newIndex].focus();
+
+        event.preventDefault();
+      }
+      else if (key.match(/35|36/)) {
+        // 35 = End, 36 = Home keyboard operations
+        switch (key) {
+          // Go to first accordion
+          case '36':
+            triggers[0].focus();
+            break;
+            // Go to last accordion
+          case '35':
+            triggers[triggers.length - 1].focus();
+            break;
+        }
+
+        event.preventDefault();
+      }
+    }
+    else if (ctrlModifier) {
+      // Control + Page Up/ Page Down keyboard operations
+      // Catches events that happen inside of panels
+      panels.forEach(function (panel, index) {
+        if (panel.contains(target)) {
+          triggers[index].focus();
+
+          event.preventDefault();
+        }
+      });
+    }
+  });
+
+});
