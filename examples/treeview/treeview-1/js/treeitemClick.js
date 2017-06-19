e1a98818e547762c520eafeb5c3b50681b421496
Jon Gunderson <jongund@illinois.edu>
2017-06-19 11:01:28 -0500

    Fixed focus styling and CSS content bugs in treeviw

--- examples/treeview/treeview-1/js/treeitemClick.js
+++ examples/treeview/treeview-1/js/treeitemClick.js
@@ -19,14 +19,17 @@ window.addEventListener('load', function () {
 
   var treeitems = document.querySelectorAll('[role="treeitem"]');
 
+
   for (var i = 0; i <treeitems.length; i++) {
 
     treeitems[i].addEventListener('click', function (event) {
+      var label = '';
       var treeitem = event.currentTarget;
-      var label = treeitem.getAttribute('aria-label');
-      if (!label) {
-        label = treeitem.innerHTML;
+      if (treeitem.getAttribute('aria-expanded')) {
+        label += 'Folder: '
+        treeitem = treeitem.firstElementChild;
       }
+      label += treeitem.innerHTML.trim();
 
       document.getElementById('last_action').value = label.trim();
 
