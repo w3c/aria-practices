55afcf7f4ad9e051acc8e6c6ceb9635b74249d97
Michiel Bijl <Michiel@Moiety.local>
2017-07-17 23:58:53 +0100

    Fix switch functionality

--- examples/switch/js/switch.js
+++ examples/switch/js/switch.js
@@ -1,13 +1,24 @@
-let HTMLSwitches = [].slice.call(document.querySelectorAll('[role^switch]'));
+// Run the load function after all resources are loaded.
+window.onload = load;
 
-for (HTMLSwitch in HTMLSwitches) {
+function load() {
+  // Create an array of all the switches on the page.
+  // The query looks for elements with a role attribute that starts with "switch".
+  let HTMLSwitches = [].slice.call(document.querySelectorAll('[role^=switch]'));
+
+  // Add an event listener to all the switches.
+  for (let HTMLSwitch of HTMLSwitches) {
     HTMLSwitch.addEventListener('click', activateSwitch);
+  }
 }
 
 function activateSwitch (event) {
-    let switchEl = event.target;
-    let currentState = switchEl.getAttribute('aria-checked');
-    let newState = (currentState === "false" ? "true" : "false");
+  let switchEl = this;
+  // Get the current state for the switch.
+  let currentState = switchEl.getAttribute('aria-checked');
+  // Invert the state.
+  let newState = (currentState === "false" ? "true" : "false");
 
-    switchEl.setAttribute('aria-checked', newState);
+  // Set the new state.
+  switchEl.setAttribute('aria-checked', newState);
 }
