639bbda92b865b00930ae40bdd75af1c7597899e
Michiel Bijl <Michiel@Moiety.local>
2017-07-20 13:18:41 +0100

    Add keyboard support

--- examples/switch/js/switch.js
+++ examples/switch/js/switch.js
@@ -1,6 +1,11 @@
 // Run the load function after all resources are loaded.
 window.onload = load;
 
+const keys = {
+  enter: 13,
+  space: 32
+};
+
 function load() {
   // Create an array of all the switches on the page.
   // The query looks for elements with a role attribute that starts with "switch".
@@ -9,11 +14,29 @@ function load() {
   // Add an event listener to all the switches.
   for (let HTMLSwitch of HTMLSwitches) {
     HTMLSwitch.addEventListener('click', activateSwitch);
+    HTMLSwitch.addEventListener('keydown', HTMLSwitchKeyHandler);
   }
-}
+};
 
-function activateSwitch (event) {
+function HTMLSwitchKeyHandler (event) {
   let switchEl = this;
+  let key = event.keyCode;
+
+  // If either enter or space is pressed activate the switch.
+  if (key === keys.enter || key === keys.space) {
+    event.preventDefault();
+    activateSwitch(event, this);
+  }
+};
+
+function activateSwitch (event, eventTarget) {
+  let switchEl = eventTarget;
+
+  // If no element is provided set it 'this'.
+  if (eventTarget === undefined) {
+    switchEl = this;
+  }
+  
   // Get the current state for the switch.
   let currentState = switchEl.getAttribute('aria-checked');
   // Invert the state.
@@ -21,4 +44,4 @@ function activateSwitch (event) {
 
   // Set the new state.
   switchEl.setAttribute('aria-checked', newState);
-}
+};
