5dd28390e3beeb433c69eff17fe06b6bafad12ce
michael-n-cooper <cooper@w3.org>
2017-10-06 20:30:46 +0000

    Generated by TRAVIS-CI

---
+++ examples/listbox/js/listbox-scrollable.js
@@ -0,0 +1,9 @@
+/**
+ * ARIA Scrollable Listbox Example
+ * @function onload
+ * @desc Initialize the listbox example once the page has loaded
+ */
+
+window.addEventListener('load', function () {
+  var exListbox = new aria.Listbox(document.getElementById('ss_elem_list'));
+});
