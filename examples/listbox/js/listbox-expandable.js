ea42023d4bbc9523a30e9ab975ba200a5ed677eb
Matt King <a11yThinker@gmail.com>
2017-10-06 15:09:46 -0700

    Expandable Listbox Example: Add example page and loading js

---
+++ examples/listbox/js/listbox-expandable.js
@@ -0,0 +1,9 @@
+/**
+ * ARIA Expandable Dropdown Listbox Example
+ * @function onload
+ * @desc Initialize the listbox example once the page has loaded
+ */
+
+window.addEventListener('load', function () {
+  var exListbox = new aria.Listbox(document.getElementById('ss_elem_list'));
+});
