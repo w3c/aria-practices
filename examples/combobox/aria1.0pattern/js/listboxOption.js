b323cfe470c47c93e0c1295fdc6542ab7f6947f4
Matt King <a11yThinker@gmail.com>
2017-10-20 15:48:32 -0700

    Fix ESLint errors

--- examples/combobox/aria1.0pattern/js/listboxOption.js
+++ examples/combobox/aria1.0pattern/js/listboxOption.js
@@ -32,20 +32,20 @@ var Option = function (domNode, listboxObj) {
   this.textComparision = domNode.textContent.toLowerCase();
 
   this.keyCode = Object.freeze({
-    'BACKSPACE':  8,
-    'TAB':        9,
-    'RETURN':    13,
-    'ESC':       27,
-    'SPACE':     32,
-    'PAGEUP':    33,
-    'PAGEDOWN':  34,
-    'END':       35,
-    'HOME':      36,
-    'LEFT':      37,
-    'UP':        38,
-    'RIGHT':     39,
-    'DOWN':      40,
-    'DELETE':    46
+    'BACKSPACE': 8,
+    'TAB': 9,
+    'RETURN': 13,
+    'ESC': 27,
+    'SPACE': 32,
+    'PAGEUP': 33,
+    'PAGEDOWN': 34,
+    'END': 35,
+    'HOME': 36,
+    'LEFT': 37,
+    'UP': 38,
+    'RIGHT': 39,
+    'DOWN': 40,
+    'DELETE': 46
   });
 };
 
