879b88e7a50f925e23ff274ea02c1b9c12008e5f
Mike Pennisi <mike@mikepennisi.com>
2018-06-21 21:52:55 -0400

    Add initial implementation of port scanning

--- test/index.js
+++ test/index.js
@@ -3,25 +3,14 @@
 const path = require('path');
 const { test } = require('ava');
 const webdriver = require('webdriverio');
-const { spawn } = require('child_process');
 
-const startGeckoDriver = (port) => {
-  const geckoDriver = path.join(
-    __dirname, '..', 'node_modules', '.bin', 'geckodriver'
-  );
-  return new Promise((resolve) => {
-    const child = spawn(geckoDriver, ['--port', port]);
+const geckodriver = require('./util/geckodriver');
 
-	// TODO fix this
-	setTimeout(() => resolve(child), 1000);
-  });
-};
-let driver;
+let driver, stopGeckoDriver;
 
 test.before(async (t) => {
-  const port = Math.round(1024 + Math.random() * 1000);
-
-  await startGeckoDriver(port);
+  const { stop, port } = await geckodriver(1022, 12 * 1000);
+  stopGeckoDriver = stop;
   driver = webdriver.remote({
 	port,
 	path: '/',
@@ -31,13 +20,14 @@ test.before(async (t) => {
   }).init();
   await driver;
 });
+
 test.beforeEach((t) => {
   t.context.driver = driver;
 });
-test.after(() => console.log('end server'));
 
 test.after.always(() => {
-  return driver.end();
+  return Promise.resolve(driver && driver.end())
+	.then(() => stopGeckoDriver && stopGeckoDriver());
 });
 
 module.exports = { test };
