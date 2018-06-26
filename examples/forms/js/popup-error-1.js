fcc693a1c1f645ab68f3159828bb957844c4af16
Jon Gunderson <jongund@illinois.edu>
2018-06-26 11:18:38 -0500

    updated JS and CSS code

--- examples/forms/js/popup-error-1.js
+++ examples/forms/js/popup-error-1.js
@@ -34,9 +34,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
-var clicked = false;
+
 // Scripting for inline form validation
-function checkItem (id, flag,message) {
+function checkItem (id, flag, message) {
   var em = document.getElementById(id + '-error');
   em.innerHTML = '';
   var ei = document.getElementById(id);
@@ -55,155 +55,201 @@ function checkItem (id, flag,message) {
   return flag;
 }
 
-function checkName () {
+function checkName (isItemEmpty) {
+  if (typeof isItemEmpty !== 'boolean') {
+    isItemEmpty = false;
+  }
   var ei = document.getElementById('id-name');
-  if (clicked) {
+  if (isItemEmpty) {
     return checkItem('id-name',(ei.value.length === 0),'Name cannot be empty! Enter your name.');
   }
+
+  return false;
 }
 
-function checkAddress () {
+function checkAddress (isItemEmpty) {
+  if (typeof isItemEmpty !== 'boolean') {
+    isItemEmpty = false;
+  }
   var ei = document.getElementById('id-address');
-  if (clicked) {
+  if (isItemEmpty) {
     return checkItem('id-address',(ei.value.length === 0),'Address cannot be empty! Enter your address.');
   }
+
+  return false;
 }
 
-function checkPhone () {
+function checkPhone(isItemEmpty) {
+  if (typeof isItemEmpty !== 'boolean') {
+    isItemEmpty = false;
+  }
+
   var ei = document.getElementById('id-phone');
   var phone = ei.value;
-  if (clicked) {
-    if (phone.length !== 0) {
-      p = '';
-      for (var i = 0;i < phone.length;i++) {
-        var c = phone[i];
-        if ((c >= '0') && (c <= '9')) {
-          p += c;
-        }
+
+  if (isItemEmpty && (phone.length === 0)) {
+      return checkItem('id-phone', true, 'Phone cannot be empty! Enter your phone number with <span aria-label=" a 10 digit number">(111) 222-3333 format</span>.');
+  }
+
+  if (phone.length !== 0) {
+    p = '';
+    for (var i = 0;i < phone.length;i++) {
+      var c = phone[i];
+      if ((c >= '0') && (c <= '9')) {
+        p += c;
       }
-      return checkItem('id-phone', ((p.length !== 7) && (p.length !== 10)),'Not a valid phone number, use this format (111) 222-3333.');
     }
-    else {
-      return checkItem('id-phone', ((phone.length === 0)), 'Phone Cannot be emprt! Enter your phone number with (111) 222-3333 format.');
-    }
+    return checkItem('id-phone', ((p.length !== 7) && (p.length !== 10)),'Not a valid phone number, use this format (111) 222-3333.');
   }
+
+  return false;
 }
 
-function checkMethod () {
-  var val = document.getElementById('id-delivery');
-  var str = val.options[val.selectedIndex].value;
-  if (clicked) {
-    return checkItem('id-delivery',(str === 'Choose your Method'), 'Choose the Delivery Method.');
+function checkDeliveryMethod (isItemEmpty) {
+  if (typeof isItemEmpty !== 'boolean') {
+    isItemEmpty = false;
   }
+
+  var select = document.getElementById('id-delivery');
+  var id = select.options[select.selectedIndex].id;
+  if (isItemEmpty) {
+    return checkItem('id-delivery',(id === 'id-delivery-choose'), 'Choose the Delivery Method.');
+  }
+
+  return false;
 }
 
-function checkSize () {
+function checkSize (isItemEmpty) {
+  if (typeof isItemEmpty !== 'boolean') {
+    isItemEmpty = false;
+  }
+
   var ei = document.getElementById('id-size');
   var small = document.getElementById('id-small').checked;
   var medium = document.getElementById('id-medium').checked;
   var large = document.getElementById('id-large').checked;
-  if (clicked) {
-    if (!(small || medium || large)) {
-      return checkItem('id-size', !(small || medium || large), 'Choose the size of your pizza');
-    }
-    else {
+
+  if (isItemEmpty) {
+    if (small || medium || large) {
       return checkItem('id-size', false, '');
     }
+    else {
+      return checkItem('id-size', true, 'Choose the size of your pizza');
+    }
   }
+
+  return false;
 }
 
-function checkCrust () {
-  var ei = document.getElementById('id-crust');
-  var thin = document.getElementById('id-thin');
-  var classic = document.getElementById('id-classic');
-  var deep = document.getElementById('id-deep');
-  if (clicked) {
-    if (!(thin.checked || classic.checked || deep.checked)) {
-      return checkItem('id-crust',!(thin.checked || classic.checked || deep.checked), 'Choose your Crust.');
+function checkCrust (isItemEmpty) {
+  if (typeof isItemEmpty !== 'boolean') {
+    isItemEmpty = false;
+  }
 
-    }
-    else {
+  var ei = document.getElementById('id-crust');
+  var thin = document.getElementById('id-thin').checked;
+  var classic = document.getElementById('id-classic').checked;
+  var deep = document.getElementById('id-deep').checked;
+
+  if (isItemEmpty) {
+    if (thin || classic || deep) {
       return checkItem('id-crust',false, '');
     }
+    else {
+      return checkItem('id-crust',!(thin.checked || classic.checked || deep.checked), 'Choose your Crust.');
+    }
   }
+
+  return false;
 }
+
 function submitOrder () {
-  clicked = true;
-  var nameFlag = checkName();
-  var addressFlag = checkAddress();
-  var phoneFlag = checkPhone();
-  var methodFlag = checkMethod();
-  var sizeFlag = checkSize();
-  var crustFlag = checkCrust();
-  if (nameFlag) {
+
+  function getRadioValue(name) {
+    var radioes = document.getElementsByName(name);
+    for (var i = 0; i < radioes.length;i++) {
+      if (radioes[i].checked) {
+        return radioes[i].value;
+      }
+    }
+    return false;
+  }
+
+  function messageItem(label, value) {
+    return label + ": " + value + "\n";
+  }
+
+  // Check all fields
+  var nameInvalid    = checkName(true);
+  var addressInvalid = checkAddress(true);
+  var phoneInvalid   = checkPhone(true);
+  var methodInvalid  = checkDeliveryMethod(true);
+  var sizeInvalid    = checkSize(true);
+  var crustInvalid   = checkCrust(true);
+
+  if (nameInvalid) {
     document.getElementById('id-name').focus();
   }
-  else if (addressFlag) {
+  else if (addressInvalid) {
     document.getElementById('id-address').focus();
   }
-  else if (phoneFlag) {
+  else if (phoneInvalid) {
     document.getElementById('id-phone').focus();
   }
-  else if (methodFlag) {
+  else if (methodInvalid) {
     document.getElementById('id-delivery').focus();
   }
-  else if (sizeFlag) {
+  else if (sizeInvalid) {
     document.getElementById('id-small').focus();
   }
-  else if (crustFlag) {
+  else if (crustInvalid) {
     document.getElementById('id-thin').focus();
   }
   else {
-    var price = 0;
-    var str = '';
+    var str = 'Pizza Order Summary\n\n';
 
-    if (window.confirm('Are you sure to submit')) {
-      var sizeRadio = document.getElementsByName('size');
-      var crustRadio = document.getElementsByName('crust');
-      var toppingCheckbox = document.getElementsByName('topping');
-      var toppingFlag = false;
-      if (document.getElementById('id-delivery').value === 'Home delivery') {
-        str += 'Delivery to Address: ' + document.getElementById('id-address').value + '\n';
-        price += 2;
-      }
-      else {
-        str += 'Delivery Method: N/A ' + '\n';
-      }
-      for (var i = 0; i < sizeRadio.length;i++) {
-        if (sizeRadio[i].checked) {
-          str += 'Pizza Size: ' + sizeRadio[i].value + '\n';
-          price += parseInt(sizeRadio[i].getAttribute('price'));
-        }
-      }
+    var delivery = document.getElementById('id-delivery');
 
-      for (var i = 0; i < crustRadio.length; i++) {
-        if (crustRadio[i].checked) {
-          str += 'Pizze Crust: ' + crustRadio[i].value + '\n';
-          if (crustRadio[i].value === 'Deep Dish') {
-            price += 1;
-          }
-        }
+    var toppingCheckboxes = document.getElementsByName('topping');
+    var toppings = '';
+    var toppingCount = 0;
+    for (var i = 0; i < toppingCheckboxes.length; i++) {
+      if(toppingCheckboxes[i].checked) {
+        if (toppingCount > 0) toppings += ', ';
+        toppings += toppingCheckboxes[i].value;
+        toppingCount += 1;
       }
+    }
 
-      str += 'Topping: ';
-      for (var i = 0; i < toppingCheckbox.length; i++) {
-        if (toppingCheckbox[i].checked) {
-          toppingFlag = true;
-          str += toppingCheckbox[i].value + '\n';
-        }
-      }
-      if (!toppingFlag) {
-        str += 'None' + '\n';
-      }
-      window.alert(str);
+    str += messageItem('Name',     document.getElementById('id-name').value);
+    str += messageItem('Address',  document.getElementById('id-address').value);
+    str += messageItem('Phone',    document.getElementById('id-phone').value);
+    str += messageItem('Delivery Method', delivery.options[delivery.selectedIndex].value);
+    str += messageItem('Size',     getRadioValue('size'));
+    str += messageItem('Crust',    getRadioValue('crust'));
+    if (toppingCount === 0) {
+      str += messageItem('Toppings', 'no additional toppings selected');
     }
     else {
-      window.alert('Your order has been cancelled');
+      if (toppingCount === 1) {
+        str += messageItem('Topping', toppings);
+      }
+      else {
+        str += messageItem('Toppings', toppings);
+      }
     }
+    str += messageItem('Cost',     document.getElementById('id-total-cost').value);
+
+    alert(str);
   }
 }
 
 function priceChange () {
+  var sizePrice = [7, 8.5, 10];
+  var deepDishPrice = 1.5;
+  var toppingPrice = 1.25;
+  var toppingPriceExtra = 2.00;
+
   var price = 0;
   var sizeRadio = document.getElementsByName('size');
   var crustRadio = document.getElementsByName('crust');
@@ -213,21 +259,21 @@ function priceChange () {
   }
   for (var i = 0;i < sizeRadio.length;i++) {
     if (sizeRadio[i].checked) {
-      price += parseFloat(sizeRadio[i].getAttribute('price'));
+      price += sizePrice[i];
     }
   }
   for (var i = 0; i < crustRadio.length; i++) {
     if (crustRadio[i].checked && crustRadio[i].value === 'Deep Dish') {
-      price += 1.50;
+      price += deepDishPrice;
     }
   }
   for (var i = 0; i < toppingCheckbox.length; i++) {
     if (toppingCheckbox[i].checked) {
-      if (toppingCheckbox[i].value === 'Extra Cheese' || toppingCheckbox[i].value === 'Black Olives') {
-        price += 2.00;
+      if (toppingCheckbox[i].id === 'id-x-cheese' || toppingCheckbox[i].id === 'id-black') {
+        price += toppingPriceExtra;
       }
       else {
-        price += 1.25;
+        price += toppingPrice;
       }
     }
   }
