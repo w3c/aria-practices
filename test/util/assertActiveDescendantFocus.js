9a1139558ec8646458f4286cc070ee05015b2ba7
Valerie R Young <valerie@bocoup.com>
2018-07-19 12:55:15 -0400

    Update all the assert functions

--- test/util/assertActiveDescendantFocus.js
+++ test/util/assertActiveDescendantFocus.js
@@ -1,21 +1,22 @@
 'use strict';
 
 const { By } = require('selenium-webdriver');
+const assert = require('assert');
 
 /**
- * Confirm the aria-activedescendant focus.
- * Contains 2 assertions.
+ * Assert the aria-activedescendant focus.
  *
  * @param {obj} t                  - ava execution object
  * @param {String} ariaDescendantSelector - selector for element with aria-activeDescendant set
  * @param {String} optionsSelector - selector to select list of canidate elements for focus
  * @param {Number} index           - index of element in list returned by optionsSelector with focus
  */
-module.exports = async function confirmAriaDescendantFocus (t, ariaDescendantSelector, optionsSelector, index) {
+module.exports = async function assertActiveDescendantFocus (t, ariaDescendantSelector, optionsSelector, index) {
+
   let options = await t.context.session
     .findElements(By.css(optionsSelector));
 
-  t.is(
+  assert.strictEqual(
     await options[index].getAttribute('aria-selected'),
     'true',
     'aria-selected should be on item at index ' + index + ' for items: ' + optionsSelector
@@ -23,7 +24,7 @@ module.exports = async function confirmAriaDescendantFocus (t, ariaDescendantSel
 
   let optionId = await options[index].getAttribute('id');
 
-  t.is(
+  assert.strictEqual(
     await t.context.session
       .findElement(By.css(ariaDescendantSelector))
       .getAttribute('aria-activedescendant'),
@@ -31,4 +32,5 @@ module.exports = async function confirmAriaDescendantFocus (t, ariaDescendantSel
     'aria-activedescendant should be set to ' + optionId + ' for items: ' + ariaDescendantSelector
   );
 
+  t.pass();
 };
--- test/util/assertAriaLabelledby.js
+++ test/util/assertAriaLabelledby.js
@@ -1,32 +1,46 @@
 'use strict';
 
 const { By } = require('selenium-webdriver');
+const assert = require('assert');
 
 /**
  * Confirm the aria-labelledby element.
- * Contains 2 assertions.
  *
  * @param {obj} t                  - ava execution object
  * @param {String} exampleId       - the id of the container element within the label must exist
  * @param {String} elementSelector - the element with aria-labelledby set
  */
 
-module.exports = async function confirmAriaLabelledby (t, exampleId, elementSelector) {
-  let labelId = await t.context.session
-    .findElement(By.css(elementSelector))
-    .getAttribute('aria-labelledby');
+module.exports = async function assertAriaLabelledby (t, exampleId, elementSelector) {
+  let element = await t.context.session
+    .findElement(By.css(elementSelector));
 
-  t.truthy(
+  let ariaLabelledbyExists = await t.context.session.executeScript(async function () {
+    selector = arguments[0];
+    let el = document.querySelector(selector);
+    return el.hasAttribute('aria-labelledby');
+  }, elementSelector);
+
+  assert(
+    ariaLabelledbyExists,
+    '"aria-labelledby" attribute should exist on element: ' + elementSelector
+  );
+
+  let labelId = await element.getAttribute('aria-labelledby');
+
+  assert.ok(
     labelId,
-    '"aria-labelledby" attribute should exist on: ' + elementSelector
+    '"aria-labelledby" attribute should have a value on element: ' + elementSelector
   );
 
   let labelElement = await t.context.session
     .findElement(By.id(exampleId))
     .findElement(By.id(labelId));
 
-  t.truthy(
+  assert.ok(
     await labelElement.getText(),
     'Element with id "' + labelId + '" should contain label text in example: ' + exampleId
   );
+
+  t.pass();
 };
