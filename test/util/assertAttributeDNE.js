d9df1b54c26af681441326390e11d39ae238cd87
Valerie R Young <valerie@bocoup.com>
2018-08-01 12:53:01 -0400

    Add assertAttributeDNE util

---
+++ test/util/assertAttributeDNE.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { By } = require('selenium-webdriver');
+const assert = require('assert');
+
+/**
+ * Assert a given attribute does not exist on all elements selected by selector
+ *
+ * @param {obj} t            - ava execution object
+ * @param {String} selector  - elements to test
+ * @param {String} attribute - attribute that should not exist
+ */
+module.exports = async function assertAttributeDNE (t, selector, attribute) {
+
+  const numElements = (await t.context.session.findElements(By.css(selector))).length;
+
+  for (let index = 0; index < numElements; index++) {
+    const attributeExists = await t.context.session.executeScript(function () {
+      let [selector, index, attribute] = arguments;
+      let elements = document.querySelectorAll(selector);
+      return elements[index].hasAttribute(attribute);
+    }, selector, index, attribute)
+
+    assert(
+      !attributeExists,
+      'Attribute "' + attribute + '" should not exist on element at index ' + index +
+        ' of elements found by selector "' + selector + '"'
+    );
+  }
+
+  t.pass();
+};
