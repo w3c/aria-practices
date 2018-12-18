8702d9b6c3573ad178f77cc2e1715e469ee96683
Michael Cooper <cooper@w3.org>
2018-12-18 13:55:36 -0500

    HTML-AAM ref

--- respec-config.js
+++ respec-config.js
@@ -112,6 +112,12 @@ var respecConfig = {
     'FPWD': 'https://www.w3.org/TR/core-aam-1.2/',
     'REC': 'https://www.w3.org/TR/core-aam-1.2/'
   },
+  htmlMappingURLs: {
+    'ED': 'https://w3c.github.io/html-aam/',
+    'WD': 'https://www.w3.org/TR/html-aam-1.0/',
+    'FPWD': 'https://www.w3.org/TR/html-aam-1.0/',
+    'REC': 'https://www.w3.org/TR/html-aam-1.0/'
+  },
 
   // alternateFormats: [
   //   {
