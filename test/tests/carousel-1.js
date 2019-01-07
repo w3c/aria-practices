6849676cb04241944b7461111bf9072252be1d4a
Jon Gunderson <jongund@illinois.edu>
2019-01-07 14:16:17 -0600

    added roledescription to region and added tests for roledescription attribute

--- test/tests/carousel-1.js
+++ test/tests/carousel-1.js
@@ -48,6 +48,12 @@ ariaTest('Carousel 1: section has aria-label', exampleFile, 'carousel-region-rol
   await assertAriaLabelExists(t, ex.landmarkSelector);
 });
 
+ariaTest('Carousel 1: section has aria-roledescription set to carousel', exampleFile, 'carousel-region-aria-roledescription', async (t) => {
+  t.plan(1);
+
+  // check the aria-roledescrption set to carousel
+  await assertAttributeValues(t, ex.landmarkSelector, 'aria-roledescription', 'carousel');
+});
 
 ariaTest('Carousel 1: previous and next buttons have role button', exampleFile, 'carousel-button-role-overrides-link', async (t) => {
   t.plan(1);
@@ -72,6 +78,13 @@ ariaTest('Carousel 1: slide containers have role group', exampleFile, 'carousel-
 ariaTest('Carousel 1: slide containers have aria-label', exampleFile, 'carousel-aria-label-number-of-slide', async (t) => {
   t.plan(1);
   await assertAriaLabelExists(t, ex.slideSelector);
+});
+
+ariaTest('Carousel 1: slide containers have aria-roledescription set to slide', exampleFile, 'carousel-aria-roledescription', async (t) => {
+  t.plan(1);
+
+  // check the aria-roledescrption set to carousel
+  await assertAttributeValues(t, ex.slideSelector, 'aria-roledescription', 'slide');
 });
 
 ariaTest('Carousel 1: slide containers have aria-describedby', exampleFile, 'carousel-aria-describedby', async (t) => {
