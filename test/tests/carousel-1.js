1379c5233cdf34f6b2abafe8695fbe1829ee5ce4
Jon Gunderson <jongund@illinois.edu>
2019-01-14 13:14:22 -0600

    removed aria-describedby from each slide

--- test/tests/carousel-1.js
+++ test/tests/carousel-1.js
@@ -16,6 +16,7 @@ const exampleFile = 'carousel/carousel-1/carousel-1.html';
 const ex = {
   landmarkSelector: '#myCarousel',
   previousNextButtonSelector: '#ex1 .carousel-control',
+  slideContainerSelector: '#ex1 .carousel-items',
   slideSelector: '#ex1 .carousel-item'
 };
 
@@ -34,39 +35,42 @@ ariaTest('Carousel 1: section has aria-roledescription set to carousel', example
   await assertAttributeValues(t, ex.landmarkSelector, 'aria-roledescription', 'carousel');
 });
 
-ariaTest('Carousel 1: previous and next buttons have role button', exampleFile, 'carousel-button-role-overrides-link', async (t) => {
+ariaTest('Carousel 1: previous and next buttons have role button', exampleFile, 'carousel-button-role-link', async (t) => {
   t.plan(1);
   await assertAriaRoles(t, 'myCarousel', 'button', 2, 'a');
 });
 
-ariaTest('Carousel 1: previous and next buttons have aria-label', exampleFile, 'carousel-aria-label-next-previous', async (t) => {
+ariaTest('Carousel 1: previous and next buttons have aria-label', exampleFile, 'carousel-button-aria-label-next-previous', async (t) => {
   t.plan(1);
   await assertAriaLabelExists(t, ex.previousNextButtonSelector);
 });
 
-ariaTest('Carousel 1: previous and next buttons have aria-controls', exampleFile, 'carousel-aria-controls', async (t) => {
+ariaTest('Carousel 1: previous and next buttons have aria-controls', exampleFile, 'carousel-button-aria-controls', async (t) => {
   t.plan(1);
   await assertAriaControls(t, ex.previousNextButtonSelector);
 });
 
-ariaTest('Carousel 1: slide containers have role group', exampleFile, 'carousel-group-role', async (t) => {
+ariaTest('Carousel 1: slide container have aria-live initially set to off', exampleFile, 'carousel-group-aria-roledescription', async (t) => {
+  t.plan(1);
+
+  // check the aria-roledescrption set to carousel
+  await assertAttributeValues(t, ex.slideContainerSelector, 'aria-live', 'off');
+});
+
+ariaTest('Carousel 1: slides have role group', exampleFile, 'carousel-group-role', async (t) => {
   t.plan(1);
   await assertAriaRoles(t, 'myCarousel', 'group', 6, 'div');
 });
 
-ariaTest('Carousel 1: slide containers have aria-label', exampleFile, 'carousel-aria-label-number-of-slide', async (t) => {
+ariaTest('Carousel 1: slides have aria-label', exampleFile, 'carousel-group-aria-label', async (t) => {
   t.plan(1);
   await assertAriaLabelExists(t, ex.slideSelector);
 });
 
-ariaTest('Carousel 1: slide containers have aria-roledescription set to slide', exampleFile, 'carousel-aria-roledescription', async (t) => {
+ariaTest('Carousel 1: slides have aria-roledescription set to slide', exampleFile, 'carousel-group-aria-roledescription', async (t) => {
   t.plan(1);
 
   // check the aria-roledescrption set to carousel
   await assertAttributeValues(t, ex.slideSelector, 'aria-roledescription', 'slide');
 });
 
-ariaTest('Carousel 1: slide containers have aria-describedby', exampleFile, 'carousel-aria-describedby', async (t) => {
-  t.plan(1);
-  await assertAriaDescribedby(t, ex.slideSelector);
-});
