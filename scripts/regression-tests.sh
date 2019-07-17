98e33df2137eda5be1d3f8771f91b867cb0233e9
Valerie R Young <valerie@bocoup.com>
2019-07-17 14:48:12 -0400

    Only run regression tests for edited examples

---
+++ scripts/regression-tests.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qP '(test/|examples/)'
+then
+  echo "Examples files were not updated, not running example regression tests."
+  exit
+fi
+
+TEST_DIRS=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -oP 'test/tests/\K[\w-]+(?=_)' | uniq)
+EXAMPLE_DIRS=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -oP 'examples/\K[\w-]+(?=/)' | uniq)
+
+# Only add match args if the example/js or example/css directories or test/index.hs
+# or the test/utils.js directories have not been edited
+
+TEST_INFRA=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -oP 'test/(util|index)')
+EXAMPLE_INFRA=$(echo "$EXAMPLE_DIRS" | grep -P '^(js|css)$')
+
+ARGS=''
+
+if [ -z $TEST_INFRA ] && [ -z $EXAMPLE_INFRA ]
+then
+  for D in $EXAMPLE_DIRS
+  do
+    ARGS="${ARGS} --match ${D}/*"
+  done
+
+  for F in $TEST_DIRS
+  do
+    ARGS="${ARGS} --match ${F}/*"
+  done
+fi
+
+AVACMD="ava -t -c 1 test/tests ${ARGS}"
+echo "$ $AVACMD"
+
+$AVACMD
