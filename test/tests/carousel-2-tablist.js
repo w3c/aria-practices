8acf97457836b60fe589aab183c9bbe07f61892c
Jon <jongund@illinois.edu>
2019-08-28 11:00:40 -0500

    added tests to example and added semi-transparent background to tablist controls

--- test/tests/carousel-2-tablist.js
+++ test/tests/carousel-2-tablist.js
@@ -9,13 +9,13 @@ const assertAriaLabelExists = require('../util/assertAriaLabelExists');
 const assertAriaRoles = require('../util/assertAriaRoles');
 const assertTabOrder = require('../util/assertTabOrder');
 
-const exampleFile = 'carousel/carousel-2/carousel-2.html';
+const exampleFile = 'carousel/carousel-2-tablist.html';
 
 const ex = {
-  pauseSelector: '#ex1 .pause',
-  nextSelector: '#ex1 .next',
-  previousSelector: '#ex1 .previous',
-  landmarkSelector: '#myCarousel'
+  rotationSelector: '#ex1 .rotation',
+  landmarkSelector: '#myCarousel',
+  rotationLabelPlaying: 'Stop automatic slide show',
+  rotationLabelPaused: 'Start automatic slide show'
 };
 
 const openTabAtIndex = async function (t, index) {
@@ -42,12 +42,20 @@ const waitAndCheckAriaSelected = async function (t, index) {
 
 // Attributes
 
-ariaTest('Carousel 2: pause button has aria-pressed', exampleFile, 'pause-aria-pressed', async (t) => {
-  t.plan(1);
-  await assertAttributeValues(t, ex.pauseSelector, 'aria-pressed', 'false');
+ariaTest('Carousel 2: rotation button has aria-label that is updated based on pause state', exampleFile, 'rotation-aria-label', async (t) => {
+  t.plan(3);
+  await assertAriaLabelExists(t, ex.rotationSelector);
+  await assertAttributeValues(t, ex.rotationSelector, 'aria-label', ex.rotationLabelPlaying);
+
+  let rotationButtonEl = await t.context.session.findElement(By.css(ex.rotationSelector));
+
+  // Send ENTER and wait for change of 'aria-label'
+  await rotationButtonEl.sendKeys(Key.ENTER);
+  await t.context.session.wait(async function () {
+    return rotationButtonEl.getAttribute('aria-label') !== ex.rotationLabelPlaying;
+  }, t.context.waitTime, 'Timeout waiting for rotation button\'s aria-label to change');
+
+  await assertAttributeValues(t, ex.rotationSelector, 'aria-label', ex.rotationLabelPaused);
+
 });
 
-ariaTest('Carousel 2: section has aria-label', exampleFile, 'landmark-aria-label', async (t) => {
-  t.plan(1);
-  await assertAriaLabelExists(t, ex.landmarkSelector);
-});
