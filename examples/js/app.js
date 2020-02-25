d2c5dc2badc6082e6a2ca1b50d62ddf57d89d64e
Zoë Bijl <5457769+ZoeBijl@users.noreply.github.com>
2020-02-25 18:45:59 +0100

    Fetch setup

--- examples/js/app.js
+++ examples/js/app.js
@@ -1,4 +1,14 @@
 (function () {
   // Load syntax highlighting
   hljs.initHighlightingOnLoad();
+
+
+  fetch('notice.html')
+    .then((response) => {
+      console.log(response.getElementById('support-notice'))
+    })
+
+  let exampleHeader = document.getElementById('ex_label')
+  let template = document.importNode('notice.html#support-notice', true)
+  console.log('loaded', template, exampleHeader)
 }());
