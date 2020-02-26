ba11bf977a47d1c90f6ceece17a4ce8ff302b054
Zoë Bijl <zoe.bijl@crowdstrike.com>
2020-02-26 18:49:17 +0100

    Pull addSupportNotice to its own function

--- examples/js/app.js
+++ examples/js/app.js
@@ -3,20 +3,24 @@
   hljs.initHighlightingOnLoad();
 
   // Add support notice to all examples
-  fetch('/examples/js/notice.html')
-  .then(function(response) {
-    return response.text()
-  })
-  .then(function(html) {
-    var parser = new DOMParser()
-    return parser.parseFromString(html, "text/html")
-  })
-  .then(function(templateResponse) {
-    var templateFragment = templateResponse.getElementById('support-notice')
-    return document.importNode(templateFragment.content, true)
-  })
-  .then(function(fragment) {
-    var exLabel = document.getElementById('ex_label')
-    exLabel.parentNode.insertBefore(fragment, exLabel.nextSibling)
-  })
+  addSupportNotice()
+
+  function addSupportNotice() {
+    fetch('/examples/js/notice.html')
+    .then(function(response) {
+      return response.text()
+    })
+    .then(function(html) {
+      var parser = new DOMParser()
+      return parser.parseFromString(html, "text/html")
+    })
+    .then(function(templateResponse) {
+      var templateFragment = templateResponse.getElementById('support-notice')
+      return document.importNode(templateFragment.content, true)
+    })
+    .then(function(fragment) {
+      var exLabel = document.getElementById('ex_label')
+      exLabel.parentNode.insertBefore(fragment, exLabel.nextSibling)
+    })
+  }
 }());
