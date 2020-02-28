7949844bc2c9aae04b4eb5da2a16993045a5a024
Zoë Bijl <5457769+ZoeBijl@users.noreply.github.com>
2020-02-28 02:31:49 +0100

    Add comments

--- examples/js/app.js
+++ examples/js/app.js
@@ -8,18 +8,23 @@
   function addSupportNotice() {
     fetch('/examples/js/notice.html')
     .then(function(response) {
+      // Return notice.html as text
       return response.text()
     })
     .then(function(html) {
+      // Parse response as text/html
       var parser = new DOMParser()
       return parser.parseFromString(html, "text/html")
     })
     .then(function(templateResponse) {
+      // Get the template element from the parsed response
       var templateFragment = templateResponse.getElementById('support-notice')
+      // Import the template contents
       return document.importNode(templateFragment.content, true)
     })
     .then(function(fragment) {
       var exLabel = document.getElementById('ex_label')
+      // Insert the support notice after the page's example heading
       exLabel.parentNode.insertBefore(fragment, exLabel.nextSibling)
     })
   }
