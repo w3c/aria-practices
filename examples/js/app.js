34b4acc3a03abe5ad31496a17806f07c4d7ce83c
Simon Pieters <zcorpan@gmail.com>
2020-05-28 16:45:18 +0200

    Fix relative urls, add 'note' class for styling, fix race condition bug and remove unnecessary use of template element

--- examples/js/app.js
+++ examples/js/app.js
@@ -3,36 +3,49 @@
   hljs.initHighlightingOnLoad();
 
   // Add support notice to all examples
-  addSupportNotice()
+  window.addEventListener('DOMContentLoaded', addSupportNotice, false);
 
   function addSupportNotice() {
+    // The "Example" heading
+    var exampleHeading = document.getElementById('ex_label');
+    if (!exampleHeading) {
+      return;
+    }
+
+    // The #browser_and_AT_support link
+    var supportLink = document.querySelector('a[href$="#browser_and_AT_support"]');
+    if (!supportLink) {
+      return;
+    }
+
+    // Get the right relative URL to the root aria-practices page
+    var urlPrefix = supportLink.getAttribute('href').split('#')[0];
+
     // Expected outcome '../js/app.js' OR '../../js/app.js'
-    var scriptSource = document.querySelector('[src$="app.js"]').getAttribute('src')
-    // Split off the 'app.js' part so we know where to fetch our template
-    var jsPath = scriptSource.split('app.js')[0]
-    // Append the template filename to the path
-    var fetchSource = jsPath + 'notice.html'
+    var scriptSource = document.querySelector('[src$="app.js"]').getAttribute('src');
+    // Replace 'app.js' part with 'notice.html'
+    var fetchSource = scriptSource.replace('app.js', 'notice.html');
 
     fetch(fetchSource)
     .then(function(response) {
       // Return notice.html as text
-      return response.text()
+      return response.text();
     })
     .then(function(html) {
       // Parse response as text/html
-      var parser = new DOMParser()
-      return parser.parseFromString(html, "text/html")
+      var parser = new DOMParser();
+      return parser.parseFromString(html, "text/html");
     })
-    .then(function(templateResponse) {
-      // Get the template element from the parsed response
-      var templateFragment = templateResponse.getElementById('support-notice')
-      // Import the template contents
-      return document.importNode(templateFragment.content, true)
-    })
-    .then(function(fragment) {
-      var exLabel = document.getElementById('ex_label')
+    .then(function(doc) {
+      // Get the details element from the parsed response
+      var noticeElement = doc.querySelector('details');
+      // Rewrite links with the right urlPrefix
+      var links = doc.querySelectorAll('a[href^="/#"]');
+      for (var i = 0; i < links.length; ++i) {
+        links[i].pathname = urlPrefix;
+      }
       // Insert the support notice after the page's example heading
-      exLabel.parentNode.insertBefore(fragment, exLabel.nextSibling)
+      exampleHeading.parentNode.insertBefore(noticeElement, exampleHeading.nextSibling);
     })
   }
 }());
