1b5e8052b8dd949416cde8bd2cb3b8c64530c310
Valerie Young <valerie@bocoup.com>
2020-07-23 11:00:10 -0700

    Update CI's regression script to list test files explicitly

--- scripts/regression-tests.sh
+++ scripts/regression-tests.sh
@@ -6,35 +6,47 @@ then
   exit
 fi
 
-TEST_DIRS=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -oP 'test/tests/\K[\w-]+(?=_)' | uniq)
-EXAMPLE_DIRS=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -oP 'examples/\K[\w-]+(?=/)' | uniq)
-
-# Only add match args if the example/js or example/css directories or test/index.hs or the
-# test/utils.js directories have not been edited. If they have been edited, run all tests.
+AVACMD="npm run regression -- -t"
+ARGS=''
 
 TEST_INFRA=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -oP 'test/(util|index)')
 EXAMPLE_INFRA=$(echo "$EXAMPLE_DIRS" | grep -P '^(js|css)$')
 
-ARGS=''
 
-if [ -z $TEST_INFRA ] && [ -z $EXAMPLE_INFRA ]
+if [ $TEST_INFRA -o $EXAMPLE_INFRA ]
 then
+
+    # If the example/js or example/css directories or test/index.js or the test/utils.js
+    # files have been edited, run all tests.
+
+    ARGS="test/tests/*.js"
+
+else
+
+  # Otherwise, run only relevant tests
+
+  TEST_FILES=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -oP 'test/tests/\K.*' | uniq)
+  EXAMPLE_DIRS=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -oP 'examples/\K[\w-]+(?=/)' | uniq)
+
+  echo TEST_FILES $TEST_FILES
+  echo EXAMPLE_DIRS $EXAMPLE_DIRS
+
   for D in $EXAMPLE_DIRS
   do
-    # Remove this if statement if we add regression tests for landmark pages
+    # Remove this if statement when we add regression tests for landmark pages
     if [ $D != 'landmarks' ]
     then
-      ARGS="${ARGS} --match ${D}/*"
+      ARGS="${ARGS} test/tests/${D}*.js"
     fi
   done
 
-  for F in $TEST_DIRS
+  for F in $TEST_FILES
   do
-    ARGS="${ARGS} --match ${F}/*"
+    ARGS="${ARGS} test/tests/${F}"
   done
+
 fi
 
-AVACMD="npm run regression -- -t -c 1 test/tests/*.js ${ARGS}"
-echo "$ $AVACMD"
+echo "$" $AVACMD $ARGS
 
-$AVACMD
+$AVACMD $ARGS
