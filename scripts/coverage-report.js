00dc62cdb016f23a066b4d9f28666e88fe1f76bc
Nick Schonning <nschonni@gmail.com>
2020-10-26 20:36:30 -0400

    typo: coberage -> coverage

--- scripts/coverage-report.js
+++ scripts/coverage-report.js
@@ -3,7 +3,7 @@
  *   This content is licensed according to the W3C Software License at
  *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
  *
- *   File:   coberage-report.js
+ *   File:   coverage-report.js
  */
 
 'use strict';
