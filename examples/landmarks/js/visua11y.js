a30239f9b3d3a477effb65f44dbca0a176edfbc7
Nick Schonning <nschonni@gmail.com>
2020-10-26 20:38:26 -0400

    typo; interal -> internal

--- examples/landmarks/js/visua11y.js
+++ examples/landmarks/js/visua11y.js
@@ -953,7 +953,7 @@ function formatInfo(info) {
 
 /*
 *   normalize: Trim leading and trailing whitespace and condense all
-*   interal sequences of whitespace to a single space. Adapted from
+*   internal sequences of whitespace to a single space. Adapted from
 *   Mozilla documentation on String.prototype.trim polyfill. Handles
 *   BOM and NBSP characters.
 */
