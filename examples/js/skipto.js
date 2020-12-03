182ff4626c031c4f2113bf1bab4c6be602aec3a4
Jon Gunderson <jongund@illinois.edu>
2020-12-03 11:21:06 -0600

    use NAVIGATION rather than NAV for navigation landmarks in menu

--- examples/js/skipto.js
+++ examples/js/skipto.js
@@ -1,4 +1,4 @@
-/*! skipto - v3.1.0 - 2020-12-01
+/*! skipto - v3.1.0 - 2020-12-03
  * https://github.com/paypal/skipto
  * Copyright (c) 2020 PayPal Accessibility Team and University of Illinois; Licensed BSD */
 /*@cc_on @*/
@@ -60,7 +60,7 @@
       headingLevelLabel: 'Heading level',
       mainLabel: 'main',
       searchLabel: 'search',
-      navLabel: 'nav',
+      navLabel: 'navigation',
       regionLabel: 'region',
       asideLabel: 'aside',
       footerLabel: 'footer',
