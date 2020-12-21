a32a1a6eafc49546c7aa10d9909c47297178456d
Valerie Young <valerie@bocoup.com>
2020-12-21 14:23:43 -0800

    issue 1619: Remove 'blur' event on combobox

--- examples/combobox/js/combobox-autocomplete.js
+++ examples/combobox/js/combobox-autocomplete.js
@@ -57,7 +57,12 @@ class ComboboxAutocomplete {
       'focus',
       this.onComboboxFocus.bind(this)
     );
-    this.comboboxNode.addEventListener('blur', this.onComboboxBlur.bind(this));
+
+    document.body.addEventListener(
+      'mouseup',
+      this.onBackgroundMouseUp.bind(this),
+      true
+    );
 
     // initialize pop up menu
 
@@ -507,11 +512,16 @@ class ComboboxAutocomplete {
     this.setCurrentOptionStyle(null);
   }
 
-  onComboboxBlur() {
-    this.comboboxHasVisualFocus = false;
-    this.setCurrentOptionStyle(null);
-    this.removeVisualFocusAll();
-    setTimeout(this.close.bind(this, false), 300);
+  onBackgroundMouseUp(event) {
+    if (
+      !this.comboboxNode.contains(event.target) &&
+      !this.listboxNode.contains(event.target)
+    ) {
+      this.comboboxHasVisualFocus = false;
+      this.setCurrentOptionStyle(null);
+      this.removeVisualFocusAll();
+      setTimeout(this.close.bind(this, true), 300);
+    }
   }
 
   onButtonClick() {
@@ -564,6 +574,5 @@ window.addEventListener('load', function () {
     var buttonNode = combobox.querySelector('button');
     var listboxNode = combobox.querySelector('[role="listbox"]');
     var cba = new ComboboxAutocomplete(comboboxNode, buttonNode, listboxNode);
-    cba.init();
   }
 });
