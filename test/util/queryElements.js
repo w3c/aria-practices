7787043f878bf6a689488ac6d7cb6f819c86163f
Jon Gunderson <jongund@illinois.edu>
2021-01-26 16:50:57 -0600

    restored test util file

--- test/util/queryElements.js
+++ test/util/queryElements.js
@@ -4,20 +4,16 @@ const { By } = require('selenium-webdriver');
  * Return an array of elements by selector. Wraps Selenium's findElements, but with a failing text for empty queries
  *
  * @param {ExecutionContext} t - Test execution context
- * @param {String} selector - CSS selector string
+ * @param {string} selector - CSS selector string
  * @param {Element} context - Element to query within, defaulting to t.context.session
- * @param {Boolean} noTest - When true, allow empty results
  *
  * @returns {Promise} Resolves to array of elements
  */
-module.exports = async function queryElements(t, selector, context, noTest) {
-  if (typeof noTest !== 'boolean') {
-    noTest = false;
-  }
+module.exports = async function queryElements(t, selector, context) {
   context = context || t.context.session;
   // eslint-disable-next-line no-restricted-properties
   const result = await context.findElements(By.css(selector));
-  if (!noTest && result.length === 0) {
+  if (result.length === 0) {
     t.fail(`Element query returned no results: ${selector}`);
   }
   return result;
