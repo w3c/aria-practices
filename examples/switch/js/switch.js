278e3d20116a8b6df8b41779fc0da9bf52f8ab20
Jon Gunderson <jongund@illinois.edu>
2021-05-05 09:20:44 -0500

    initial implementation of a switch that supports HC

--- examples/switch/js/switch.js
+++ examples/switch/js/switch.js
@@ -1,32 +1,42 @@
+/*
+ *   This content is licensed according to the W3C Software License at
+ *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+ *
+ *   File:  switch.js
+ *
+ *   Desc:  Switch widget that implements ARIA Authoring Practices
+ */
+
+'use strict';
+
 class Switch {
-  constructor (element) {
-    this.element = element
-    this.bindEvents()
-    console.log('switch')
+  constructor(domNode) {
+    this.switchNode = domNode;
+    this.switchNode.addEventListener('click', () => this.toggleStatus());
+    this.switchNode.addEventListener('keydown', (event) =>
+      this.handleKeydown(event)
+    );
   }
 
-  bindEvents () {
-    this.element.addEventListener('click', () => this.toggleStatus())
-    this.element.addEventListener('keydown', event => this.onKeydown(event))
-  }
-
-  onKeydown(event) {
+  handleKeydown(event) {
     // Only do something when space or return is pressed
     if (event.key === 'Enter' || event.key === ' ') {
-      event.preventDefault()
-      this.toggleStatus()
+      event.preventDefault();
+      this.toggleStatus();
     }
   }
 
-  // Switch state of a switch, hah, see what I did there, right…
+  // Switch state of a switch
   toggleStatus() {
-    const currentState = this.element.getAttribute('aria-checked') === 'true'
-    const newState = String(!currentState)
+    const currentState =
+      this.switchNode.getAttribute('aria-checked') === 'true';
+    const newState = String(!currentState);
 
-    this.element.setAttribute('aria-checked', newState)
+    this.switchNode.setAttribute('aria-checked', newState);
   }
 }
 
 // Initialize the Switch component on all matching DOM nodes
-Array.from(document.querySelectorAll('[role^=switch]'))
-    .forEach(element => new Switch(element))
+Array.from(document.querySelectorAll('[role^=switch]')).forEach(
+  (element) => new Switch(element)
+);
