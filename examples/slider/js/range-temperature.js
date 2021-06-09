1c78bb01e2608f83f923bb8444c5bbc6a066eea9
Jon Gunderson <jongund@illinois.edu>
2021-06-09 16:20:44 -0500

    only use input event, works on iOS and Android using touch events, but not VO or talk back

--- examples/slider/js/range-temperature.js
+++ examples/slider/js/range-temperature.js
@@ -14,35 +14,13 @@ class RangeTemperature {
     this.labelCelsiusAbbrev = '°C';
     this.labelCelsius = ' degrees Celsius';
 
-    this.inputEventCount = 0;
-    this.pointermoveEventCount = 0;
-
     this.domNode = domNode;
     this.rangeNode = domNode.querySelector('input[type="range"]');
     this.valueNode = domNode.querySelector('.range-value');
-    this.rangeNode.addEventListener('input', this.onInput.bind(this));
-    /*    this.rangeNode.addEventListener(
-      'pointermove',
-      this.onPointermove.bind(this)
-    );
-*/
+    this.rangeNode.addEventListener('input', this.handleInput.bind(this));
   }
 
-  onInput(event) {
-    this.inputEventCount += 1;
-    document.getElementById('id-input').value = this.inputEventCount;
-    this.onRangeChange(event);
-  }
-
-  onPointermove(event) {
-    this.pointermoveEventCount += 1;
-    document.getElementById(
-      'id-pointermove'
-    ).value = this.pointermoveEventCount;
-    this.onRangeChange(event);
-  }
-
-  onRangeChange(event) {
+  handleInput(event) {
     if (this.domNode.contains(event.currentTarget)) {
       let valuetext =
         parseFloat(this.rangeNode.value).toFixed(1) + this.labelCelsiusAbbrev;
