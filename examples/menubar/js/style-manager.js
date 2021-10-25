501cf9eade81b5a4faa577aa1486e18aacd098da
Simon Pieters <zcorpan@gmail.com>
2021-10-25 22:10:52 +0200

    Switch to JavaScript classes

--- examples/menubar/js/style-manager.js
+++ examples/menubar/js/style-manager.js
@@ -9,150 +9,154 @@
 
 'use strict';
 
-var StyleManager = function (node) {
-  this.node = node;
-  this.fontSize = 'medium';
-};
+/* exported StyleManager */
 
-StyleManager.prototype.setFontFamily = function (value) {
-  this.node.style.fontFamily = value;
-};
-
-StyleManager.prototype.setTextDecoration = function (value) {
-  this.node.style.textDecoration = value;
-};
-
-StyleManager.prototype.setTextAlign = function (value) {
-  this.node.style.textAlign = value;
-};
-
-StyleManager.prototype.setFontSize = function (value) {
-  this.fontSize = value;
-  this.node.style.fontSize = value;
-};
-
-StyleManager.prototype.setColor = function (value) {
-  this.node.style.color = value;
-};
-
-StyleManager.prototype.setBold = function (flag) {
-  if (flag) {
-    this.node.style.fontWeight = 'bold';
-  } else {
-    this.node.style.fontWeight = 'normal';
-  }
-};
-
-StyleManager.prototype.setItalic = function (flag) {
-  if (flag) {
-    this.node.style.fontStyle = 'italic';
-  } else {
-    this.node.style.fontStyle = 'normal';
-  }
-};
-
-StyleManager.prototype.fontSmaller = function () {
-  switch (this.fontSize) {
-    case 'small':
-      this.setFontSize('x-small');
-      break;
-
-    case 'medium':
-      this.setFontSize('small');
-      break;
-
-    case 'large':
-      this.setFontSize('medium');
-      break;
-
-    case 'x-large':
-      this.setFontSize('large');
-      break;
-
-    default:
-      break;
-  } // end switch
-};
-
-StyleManager.prototype.fontLarger = function () {
-  switch (this.fontSize) {
-    case 'x-small':
-      this.setFontSize('small');
-      break;
-
-    case 'small':
-      this.setFontSize('medium');
-      break;
-
-    case 'medium':
-      this.setFontSize('large');
-      break;
-
-    case 'large':
-      this.setFontSize('x-large');
-      break;
-
-    default:
-      break;
-  } // end switch
-};
-
-StyleManager.prototype.isMinFontSize = function () {
-  return this.fontSize === 'x-small';
-};
-
-StyleManager.prototype.isMaxFontSize = function () {
-  return this.fontSize === 'x-large';
-};
-
-StyleManager.prototype.getFontSize = function () {
-  return this.fontSize;
-};
-
-StyleManager.prototype.setOption = function (option, value) {
-  option = option.toLowerCase();
-  if (typeof value === 'string') {
-    value = value.toLowerCase();
+class StyleManager {
+  constructor(node) {
+    this.node = node;
+    this.fontSize = 'medium';
   }
 
-  switch (option) {
-    case 'font-bold':
-      this.setBold(value);
-      break;
+  setFontFamily(value) {
+    this.node.style.fontFamily = value;
+  }
 
-    case 'font-color':
-      this.setColor(value);
-      break;
+  setTextDecoration(value) {
+    this.node.style.textDecoration = value;
+  }
 
-    case 'font-family':
-      this.setFontFamily(value);
-      break;
+  setTextAlign(value) {
+    this.node.style.textAlign = value;
+  }
 
-    case 'font-smaller':
-      this.fontSmaller();
-      break;
+  setFontSize(value) {
+    this.fontSize = value;
+    this.node.style.fontSize = value;
+  }
 
-    case 'font-larger':
-      this.fontLarger();
-      break;
+  setColor(value) {
+    this.node.style.color = value;
+  }
 
-    case 'font-size':
-      this.setFontSize(value);
-      break;
+  setBold(flag) {
+    if (flag) {
+      this.node.style.fontWeight = 'bold';
+    } else {
+      this.node.style.fontWeight = 'normal';
+    }
+  }
 
-    case 'font-italic':
-      this.setItalic(value);
-      break;
+  setItalic(flag) {
+    if (flag) {
+      this.node.style.fontStyle = 'italic';
+    } else {
+      this.node.style.fontStyle = 'normal';
+    }
+  }
 
-    case 'text-align':
-      this.setTextAlign(value);
-      break;
+  fontSmaller() {
+    switch (this.fontSize) {
+      case 'small':
+        this.setFontSize('x-small');
+        break;
 
-    case 'text-decoration':
-      this.setTextDecoration(value);
-      break;
+      case 'medium':
+        this.setFontSize('small');
+        break;
 
-    default:
-      break;
-  } // end switch
-};
+      case 'large':
+        this.setFontSize('medium');
+        break;
+
+      case 'x-large':
+        this.setFontSize('large');
+        break;
+
+      default:
+        break;
+    } // end switch
+  }
+
+  fontLarger() {
+    switch (this.fontSize) {
+      case 'x-small':
+        this.setFontSize('small');
+        break;
+
+      case 'small':
+        this.setFontSize('medium');
+        break;
+
+      case 'medium':
+        this.setFontSize('large');
+        break;
+
+      case 'large':
+        this.setFontSize('x-large');
+        break;
+
+      default:
+        break;
+    } // end switch
+  }
+
+  isMinFontSize() {
+    return this.fontSize === 'x-small';
+  }
+
+  isMaxFontSize() {
+    return this.fontSize === 'x-large';
+  }
+
+  getFontSize() {
+    return this.fontSize;
+  }
+
+  setOption(option, value) {
+    option = option.toLowerCase();
+    if (typeof value === 'string') {
+      value = value.toLowerCase();
+    }
+
+    switch (option) {
+      case 'font-bold':
+        this.setBold(value);
+        break;
+
+      case 'font-color':
+        this.setColor(value);
+        break;
+
+      case 'font-family':
+        this.setFontFamily(value);
+        break;
+
+      case 'font-smaller':
+        this.fontSmaller();
+        break;
+
+      case 'font-larger':
+        this.fontLarger();
+        break;
+
+      case 'font-size':
+        this.setFontSize(value);
+        break;
+
+      case 'font-italic':
+        this.setItalic(value);
+        break;
+
+      case 'text-align':
+        this.setTextAlign(value);
+        break;
+
+      case 'text-decoration':
+        this.setTextDecoration(value);
+        break;
+
+      default:
+        break;
+    } // end switch
+  }
+}
