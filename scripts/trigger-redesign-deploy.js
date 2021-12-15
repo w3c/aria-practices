46e8e80a74831d7e5c98a5637b083fa0924d7d16
Howard Edwards <howarde.edwards@gmail.com>
2021-12-15 11:46:35 -0500

    check

--- scripts/trigger-redesign-deploy.js
+++ scripts/trigger-redesign-deploy.js
@@ -3,7 +3,7 @@ const { Octokit } = require('@octokit/rest');
 // octokit should be authenticated with GITHUB_TOKEN from GA
 console.log(process.env.GITHUB_TOKEN.substring(0, 8));
 const octokit = new Octokit({
-  auth: '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', // to redact
+  auth: process.env.GITHUB_TOKEN,
 });
 
 (async () => {
