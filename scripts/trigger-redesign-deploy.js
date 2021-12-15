e93d66edad130f1bf23c88e702588b6c0b61c3a6
Howard Edwards <howarde.edwards@gmail.com>
2021-12-15 11:53:30 -0500

    cleanup

--- scripts/trigger-redesign-deploy.js
+++ scripts/trigger-redesign-deploy.js
@@ -1,19 +1,20 @@
 const { Octokit } = require('@octokit/rest');
 
 // octokit should be authenticated with GITHUB_TOKEN from GA
-console.log(process.env.GH_TOKEN.substring(0, 8));
 const octokit = new Octokit({
   auth: process.env.GH_TOKEN,
 });
 
 (async () => {
-  let workflowDispatchResult =
+  try {
     await octokit.rest.actions.createWorkflowDispatch({
       owner: 'howard-e',
       repo: 'wai-aria-practices',
       workflow_id: 'deploy.yml',
       ref: 'master',
     });
-
-  console.info(workflowDispatchResult);
+    console.info('workflow.dispatch.success');
+  } catch (e) {
+    console.error('workflow.dispatch.fail');
+  }
 })();
