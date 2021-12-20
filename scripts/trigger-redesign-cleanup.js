4a69dde66afaeb8ad62f5efba8a551c5866702e9
Howard Edwards <howarde.edwards@gmail.com>
2021-12-20 14:33:16 -0500

    remove branch trigger

---
+++ scripts/trigger-redesign-cleanup.js
@@ -0,0 +1,23 @@
+const { Octokit } = require('@octokit/rest');
+
+// octokit should be authenticated with GITHUB_TOKEN from GA
+const octokit = new Octokit({
+  auth: process.env.GH_TOKEN,
+});
+
+(async () => {
+  try {
+    await octokit.rest.actions.createWorkflowDispatch({
+      owner: 'howard-e',
+      repo: 'wai-aria-practices',
+      workflow_id: 'remove-branch.yml',
+      ref: 'master',
+      inputs: {
+        apg_branch: process.env.APG_BRANCH,
+      },
+    });
+    console.info('workflow.dispatch.success');
+  } catch (e) {
+    console.error('workflow.dispatch.fail', e);
+  }
+})();
