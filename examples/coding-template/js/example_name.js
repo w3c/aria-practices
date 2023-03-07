c9bf8a3417aaff20e74562d88a577f8c8e1c9187
Alexander Flenniken <alflennik@users.noreply.github.com>
2023-03-07 01:23:06 -0500

    Editors Draft on github.io: Stub content and point readers to relevant pages on WAI site (#2639)
    Resolves #2420 
    
    In the gh-pages branch that is published to w3c.github.io/aria-practices (the old editor's draft), replaces content in HTML files with a stub which links to the current version on w3.org.
    
    ---------
    
    Co-authored-by: Matt King <a11yThinker@gmail.com>

--- examples/coding-template/js/example_name.js
+++
@@ -1,8 +0,0 @@
-/*
- * This content is licensed according to the W3C Software License at
- * https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
- */
-
-/*
- * Rename this file to the name of the example, e.g., checkbox.js.
- */
