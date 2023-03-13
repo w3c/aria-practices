a74962f1f829537c93de996b5d9b0ba2660cd95a
alflennik <alflennik@users.noreply.github.com>
2023-03-13 14:23:28 -0400

    Remove unneeded iframe magic number adjustment

--- content/shared/js/app.js
+++ content/shared/js/app.js
@@ -55,7 +55,6 @@
     }
     const iframe = document.querySelector(`.${data.iframe}`);
     if (!iframe) return;
-    const magicNumberAdjustment = 35;
-    iframe.style.height = `${data.height + magicNumberAdjustment}px`;
+    iframe.style.height = data.height + 'px';
   }
 })();
