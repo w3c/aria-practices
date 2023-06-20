cdb627c0d258419418cf224be20499ebf7a84025
Paul Vinson Brown <paul.brown@mukavait.fi>
2023-06-20 08:59:53 +0300

    Correct comments re: treegrid test
    Just happened upon this one - looks like someone's copy-pasting had not
    been completely finished, and comments still said "HOME", though they
    clearly were meant to say "END".

--- test/tests/treegrid_treegrid-1.js
+++ test/tests/treegrid_treegrid-1.js
@@ -722,12 +722,12 @@ ariaTest('END moves focus', exampleFile, 'key-end', async (t) => {
     'Sending END to first row should result on focus on last row'
   );
 
-  // Send HOME to first gridcell
+  // Send END to first gridcell
   await putFocusOnRow1Gridcell(t, 0);
   await sendKeyToGridcellAndWait(t, 0, 0, Key.END);
   t.true(
     await checkFocusOnGridcell(t, 0, 2),
-    "Sending HOME to first row's first gridcell should move focus to first row's last gridcell"
+    "Sending END to first row's first gridcell should move focus to first row's last gridcell"
   );
 });
 
