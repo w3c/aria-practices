f97050be97ac56ce6dc45c82a3cf90d5048b03ae
Howard Edwards <howarde.edwards@gmail.com>
2023-10-02 15:35:39 -0400

    Infrastructure: link-checker.js: Check if any of the known `pageIds` include the hash being checked for (pull #2812)
    Modifies link checker so that when GitHub dynamically prepends "user-content-" to fragment IDs that are in user-generated content, the link test will pass.

--- .link-checker.js
+++ .link-checker.js
@@ -11,9 +11,16 @@ module.exports = {
       '.carousel-image a',
     ],
   },
+  hashCheckHandlers: [
+    {
+      name: 'github',
+      pattern: /^https:\/\/github\.com\/.*/,
+      matchHash: (ids, hash) =>
+        ids.includes(hash) || ids.includes(`user-content-${hash}`),
+    },
+  ],
   ignoreHashesOnExternalPagesMatchingRegex: [
     // Some hash links are resolved with JS and are therefore difficult to check algorithmically
-    /^https:\/\/github\.com\/.*\/wiki\//,
     /^https:\/\/html\.spec\.whatwg\.org\/multipage\//,
   ],
 };
