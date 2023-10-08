cf6754012dbcbb169a579d16413e539a5c9df121
Jon Gunderson <jongund@illinois.edu>
2023-10-08 18:46:59 -0500

    fixed validation errors

--- content/patterns/slider/examples/js/slider-rating.js
+++ content/patterns/slider/examples/js/slider-rating.js
@@ -224,9 +224,9 @@ class RatingSlider {
   onRailClick(event) {
     const x = this.getSVGPoint(event).x;
     const diffX = x - this.railOffset;
-    const fract =
+    const rating =
       0.5 + (diffX * (this.valueMax - this.valueMin)) / this.railWidth;
-    const value = Math.round(fract);
+    const value = Math.round(rating);
 
     this.moveSliderTo(value);
 
