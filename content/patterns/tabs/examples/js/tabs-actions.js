2b77bc227852c7e62e8ae97d46d74b43a62a5d36
Adam Page <adamfpage@gmail.com>
2024-08-21 18:05:30 -0700

    add multi-action support and example

--- content/patterns/tabs/examples/js/tabs-actions.js
+++ content/patterns/tabs/examples/js/tabs-actions.js
@@ -2,7 +2,7 @@
  *   This content is licensed according to the W3C Software License at
  *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
  *
- *   File:   tabs-manual.js
+ *   File:   tabs-actions.js
  *
  *   Desc:   Tablist widget that implements ARIA Authoring Practices
  */
@@ -31,10 +31,22 @@ class TabsManual {
 
       tab.addEventListener('keydown', this.onKeydown.bind(this));
       tab.addEventListener('click', this.onClick.bind(this));
+      tab.addEventListener('focusout', this.onFocusout.bind(this));
 
       this.getTabAriaActions(tab).forEach((action) => {
         action.addEventListener('keydown', this.onKeydown.bind(this));
-        action.addEventListener('blur', this.onBlur.bind(this));
+        action.addEventListener('focusout', this.onFocusout.bind(this));
+
+        this.getTabAriaActionOperations(action).forEach((operation) => {
+          operation.addEventListener(
+            'click',
+            this.performTabOperation.bind(this)
+          );
+          operation.addEventListener(
+            'keydown',
+            this.performTabOperation.bind(this)
+          );
+        });
       });
 
       if (!this.firstTab) {
@@ -55,20 +67,52 @@ class TabsManual {
     }
   }
 
+  getTabAriaActionOperations(action) {
+    const operationCode = action.getAttribute('data-operation');
+    const idrefControls = action.getAttribute('aria-controls');
+    let operations = [];
+    if (operationCode) {
+      operations.push(action);
+    }
+    if (idrefControls) {
+      const context = document.getElementById(idrefControls);
+      const nodes = Array.from(context.querySelectorAll('[data-operation]'));
+      operations = operations.concat(nodes);
+    }
+    return operations;
+  }
+
   getTabAssociatedWithAction(action) {
     return document.querySelector(`[aria-actions~="${action.id}"]`);
   }
 
-  makeAssociatedActionsFocusable(tab) {
+  getClosestTabWrapper(el) {
+    return el.closest('[role="presentation"]');
+  }
+
+  getActionAssociatedWithOperation(operation) {
+    const idrefAction = operation.getAttribute('data-action');
+    if (idrefAction) {
+      return document.getElementById(idrefAction);
+    } else {
+      return operation;
+    }
+  }
+
+  makeTabeAndActionsFocusable(tab) {
+    tab.removeAttribute('tabindex');
     this.getTabAriaActions(tab).forEach((action) => {
       action.removeAttribute('tabindex');
     });
   }
 
-  makeAssociatedActionsUnfocusable(tab) {
-    this.getTabAriaActions(tab).forEach((action) => {
-      action.tabIndex = -1;
-    });
+  makeTabAndActionsUnfocusable(tab) {
+    if (tab.getAttribute('aria-selected') !== 'true') {
+      tab.tabIndex = -1;
+      this.getTabAriaActions(tab).forEach((action) => {
+        action.tabIndex = -1;
+      });
+    }
   }
 
   setSelectedTab(currentTab) {
@@ -76,13 +120,11 @@ class TabsManual {
       var tab = this.tabs[i];
       if (currentTab === tab) {
         tab.setAttribute('aria-selected', 'true');
-        tab.removeAttribute('tabindex');
-        this.makeAssociatedActionsFocusable(tab);
+        this.makeTabeAndActionsFocusable(tab);
         this.tabpanels[i].classList.remove('is-hidden');
       } else {
         tab.setAttribute('aria-selected', 'false');
-        tab.tabIndex = -1;
-        this.makeAssociatedActionsUnfocusable(tab);
+        this.makeTabAndActionsUnfocusable(tab);
         this.tabpanels[i].classList.add('is-hidden');
       }
     }
@@ -92,9 +134,9 @@ class TabsManual {
     currentTab.focus();
     this.tabs.forEach((tab) => {
       if (currentTab === tab) {
-        this.makeAssociatedActionsFocusable(tab);
+        this.makeTabeAndActionsFocusable(tab);
       } else if (tab.getAttribute('aria-selected') !== 'true') {
-        this.makeAssociatedActionsUnfocusable(tab);
+        this.makeTabAndActionsUnfocusable(tab);
       }
     });
   }
@@ -168,12 +210,60 @@ class TabsManual {
     this.setSelectedTab(event.currentTarget);
   }
 
-  onBlur(event) {
-    const tab = event.currentTarget.closest(
-      '.menu-button-actions'
-    ).previousElementSibling;
-    if (tab.getAttribute('aria-selected') !== 'true') {
-      this.makeAssociatedActionsUnfocusable(tab);
+  performTabOperation(event) {
+    if (event.type === 'keydown' && event.code !== 'Enter') {
+      return;
+    }
+
+    const operation = event.currentTarget;
+    const operationCode = operation.getAttribute('data-operation');
+    const action = this.getActionAssociatedWithOperation(operation);
+    const tab = this.getTabAssociatedWithAction(action);
+    const tabPanel = document.getElementById(tab.getAttribute('aria-controls'));
+
+    switch (operationCode) {
+      case 'clipboard-copy': {
+        const tabText = tabPanel.textContent.replace(/\s+/g, ' ').trim();
+        navigator.clipboard.writeText(tabText).then(
+          () => {
+            this.presentFeedback(
+              operation,
+              `Copied ${tab.textContent} tab contents to clipboard`
+            );
+          },
+          (err) => {
+            this.presentFeedback(
+              operation,
+              `Failed to copy ${tab.textContent} tab contents to clipboard`,
+              err
+            );
+          }
+        );
+        break;
+      }
+      default: {
+        this.presentFeedback(
+          operation,
+          `<em>Sorry, haven’t implemented the ${operationCode} operation yet</em>`,
+          'error'
+        );
+      }
+    }
+  }
+
+  presentFeedback(operation, msg, err) {
+    const output = operation.closest('.tabs').querySelector('output');
+    if (output) {
+      output.innerHTML = `<span class="${err ? 'error' : 'success'}">${msg}</span>`;
+    }
+  }
+
+  onFocusout(event) {
+    const tabWrapperOld = this.getClosestTabWrapper(event.currentTarget);
+    const tabWrapperNew = this.getClosestTabWrapper(event.relatedTarget);
+    if (tabWrapperOld !== tabWrapperNew) {
+      const tab = tabWrapperOld.querySelector('[role="tab"]');
+      this.makeTabAndActionsUnfocusable(tab);
     }
   }
 }
@@ -185,38 +275,4 @@ window.addEventListener('load', function () {
   for (var i = 0; i < tablists.length; i++) {
     new TabsManual(tablists[i]);
   }
-});
-
-
-
-function addClickHandlers() {
-  const menuitems = document.querySelectorAll('[role=menuitem]');
-  menuitems.forEach(function (menuitem) {
-    menuitem.addEventListener('click', onMenuitemClick);
-    menuitem.addEventListener('keydown', onMenuitemClick);
-  });
-}
-
-function onMenuitemClick(event) {
-  if (event.type === 'keydown' && event.code !== 'Enter') {
-    return;
-  }
-  const menuitem = event.currentTarget;
-  if (menuitem.textContent === 'Copy tab contents to clipboard') {
-    const tab = menuitem.closest('.menu-button-actions').previousElementSibling;
-    const tabPanel = document.getElementById(tab.getAttribute('aria-controls'));
-    const tabText = tabPanel.textContent.replace(/\s+/g, ' ').trim();
-    navigator.clipboard.writeText(tabText).then(
-      function () {
-        alert('Tab contents copied to clipboard');
-      },
-      function (err) {
-        alert('Error copying tab contents to clipboard: ', err);
-      }
-    );
-  }
-}
-
-window.addEventListener('load', function () {
-  addClickHandlers();
 });
