9bd20619a384e6b14e0ead74a52af2957cae6876
Jon Gunderson <jongund@illinois.edu>
2025-01-28 19:55:00 -0600

    fixing linting errors

--- content/patterns/landmarks/js/image-resizing.js
+++ content/patterns/landmarks/js/image-resizing.js
@@ -10,14 +10,14 @@
 'use strict';
 
 function resizeImagesInFigures() {
-  const figures = document.querySelectorAll('#at figure');
-  for(let i = 0; i < figures.length; i++) {
-    const image = figures[i].querySelector('img');
-    if (image) {
-      const rect = figures[i].getBoundingClientRect();
-      image.style.width = (rect.width - 16) + 'px';
+    const figures = document.querySelectorAll('#at figure');
+    for (let i = 0; i < figures.length; i++) {
+        const image = figures[i].querySelector('img');
+        if (image) {
+            const rect = figures[i].getBoundingClientRect();
+            image.style.width = (rect.width - 16) + 'px';
+        }
     }
-  }
 }
 
 window.addEventListener('load', resizeImagesInFigures);
