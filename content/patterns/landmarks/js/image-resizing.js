dd9e023d9f4f8d4a7ff3d24fb5c4d77d8062a1b7
Jon Gunderson <jongund@illinois.edu>
2025-01-28 18:36:27 -0600

    fixed linting bug

--- content/patterns/landmarks/js/image-resizing.js
+++ content/patterns/landmarks/js/image-resizing.js
@@ -1,4 +1,14 @@
-/* image-resizing.js */
+/*
+ *   This content is licensed according to the W3C Software License at
+ *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+ *
+ *   File:  image-resizing.js
+ *
+ *   Desc:   Resizes images to fit in figure elements
+ */
+
+'use strict';
+
 function resizeImagesInFigures () {
   const figures = document.querySelectorAll('#at figure');
   for(let i = 0; i < figures.length; i += 1) {
