c8d4731f6a868af5c3009e0a48c6603c7cbf9f79
Jon Gunderson <jongund@illinois.edu>
2025-06-04 13:59:11 -0500

    changed OL to UL list

--- test/tests/disclosure_faq.js
+++ test/tests/disclosure_faq.js
@@ -8,16 +8,16 @@ const exampleFile = 'content/patterns/disclosure/examples/disclosure-faq.html';
 const ex = {
   buttonSelector: '#ex1 button',
   buttonSelectors: [
-    '#ex1 ol li:nth-child(1) button',
-    '#ex1 ol li:nth-child(2) button',
-    '#ex1 ol li:nth-child(3) button',
-    '#ex1 ol li:nth-child(4) button',
+    '#ex1 ul li:nth-child(1) button',
+    '#ex1 ul li:nth-child(2) button',
+    '#ex1 ul li:nth-child(3) button',
+    '#ex1 ul li:nth-child(4) button',
   ],
   answerSelectors: [
-    '#ex1 ol li:nth-child(1) .desc',
-    '#ex1 ol li:nth-child(2) .desc',
-    '#ex1 ol li:nth-child(3) .desc',
-    '#ex1 ol li:nth-child(4) .desc',
+    '#ex1 ul li:nth-child(1) .desc',
+    '#ex1 ul li:nth-child(2) .desc',
+    '#ex1 ul li:nth-child(3) .desc',
+    '#ex1 ul li:nth-child(4) .desc',
   ],
 };
 
