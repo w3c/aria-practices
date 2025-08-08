7adc5db2b0ab207ead06b16374275507fe50b44e
Adam Page <adamfpage@gmail.com>
2025-08-07 22:58:57 -0700

    initial tests

---
+++ test/tests/spinbutton_quantity.js
@@ -0,0 +1,440 @@
+const { ariaTest } = require('..');
+const { By, Key } = require('selenium-webdriver');
+const assertAttributeValues = require('../util/assertAttributeValues');
+const assertAriaRoles = require('../util/assertAriaRoles');
+
+const exampleFile =
+  'content/patterns/spinbutton/examples/quantity-spinbutton.html';
+
+const ex = {
+  spin: {
+    id: 'adults',
+    sel: '#adults',
+    min: '1',
+    max: '8',
+    now: '1',
+  },
+
+  inc: {
+    sel: '[aria-controls="adults"][data-spinbutton-operation="increment"]',
+    accname: 'Add adult',
+  },
+
+  dec: {
+    sel: '[aria-controls="adults"][data-spinbutton-operation="decrement"]',
+    accname: 'Remove adult',
+  },
+
+  status: {
+    sel: 'div[for="adults"]',
+    selfDestruct: '2000',
+  },
+};
+
+ex.inc.symbolSel = ex.inc.sel + ' > span';
+ex.dec.symbolSel = ex.dec.sel + ' > span';
+(ex.inputScenarios = {
+  0: ex.spin.min,
+  1: ex.spin.min,
+  4: '4',
+  8: ex.spin.max,
+  13: ex.spin.max,
+  abc: ex.spin.min,
+  '-7': '7',
+}),
+  // Attributes
+
+  ariaTest(
+    'role="spinbutton" on input element',
+    exampleFile,
+    'spinbutton-role',
+    async (t) => {
+      await assertAriaRoles(t, 'example', 'spinbutton', '3', 'input');
+    }
+  );
+
+ariaTest(
+  '"aria-valuemin" represents the minimum value on spinbuttons',
+  exampleFile,
+  'spinbutton-aria-valuemin',
+  async (t) => {
+    await assertAttributeValues(t, ex.spin.sel, 'aria-valuemin', ex.spin.min);
+  }
+);
+
+ariaTest(
+  '"aria-valuemax" represents the maximum value on spinbuttons',
+  exampleFile,
+  'spinbutton-aria-valuemax',
+  async (t) => {
+    await assertAttributeValues(t, ex.spin.sel, 'aria-valuemax', ex.spin.max);
+  }
+);
+
+ariaTest(
+  '"aria-valuenow" reflects spinbutton value as a number',
+  exampleFile,
+  'spinbutton-aria-valuenow',
+  async (t) => {
+    await assertAttributeValues(t, ex.spin.sel, 'aria-valuenow', ex.spin.now);
+  }
+);
+
+ariaTest(
+  '"tabindex=-1" removes increment and decrement buttons from page tab order',
+  exampleFile,
+  'button-tabindex',
+  async (t) => {
+    await assertAttributeValues(t, ex.inc.sel, 'tabindex', '-1');
+    await assertAttributeValues(t, ex.dec.sel, 'tabindex', '-1');
+  }
+);
+
+ariaTest(
+  'increment and decrement buttons use aria-controls to reference spinner',
+  exampleFile,
+  'button-aria-controls',
+  async (t) => {
+    await assertAttributeValues(t, ex.inc.sel, 'aria-controls', ex.spin.id);
+    await assertAttributeValues(t, ex.dec.sel, 'aria-controls', ex.spin.id);
+  }
+);
+
+ariaTest(
+  '"title" provides accessible name for the increment and decrement buttons',
+  exampleFile,
+  'button-title',
+  async (t) => {
+    await assertAttributeValues(t, ex.inc.sel, 'title', ex.inc.accname);
+    await assertAttributeValues(t, ex.dec.sel, 'title', ex.dec.accname);
+  }
+);
+
+ariaTest(
+  '"aria-hidden" hides symbolic accname equivalent from screen reader users',
+  exampleFile,
+  'span-aria-hidden',
+  async (t) => {
+    await assertAttributeValues(t, ex.inc.symbolSel, 'aria-hidden', 'true');
+    await assertAttributeValues(t, ex.dec.symbolSel, 'aria-hidden', 'true');
+  }
+);
+
+ariaTest(
+  'role="status" on div element',
+  exampleFile,
+  'status-role',
+  async (t) => {
+    await assertAriaRoles(t, 'example', 'status', '3', 'div');
+  }
+);
+
+// keys
+
+ariaTest('end', exampleFile, 'spinbutton-end', async (t) => {
+  const spinner = await t.context.session.findElement(By.css(ex.spin.sel));
+  const max = parseInt(ex.spin.max);
+
+  // Send end key
+  await spinner.sendKeys(Key.END);
+  t.is(
+    parseInt(await spinner.getAttribute('aria-valuenow')),
+    max,
+    `After sending end key, aria-valuenow should be the maximum value: ${max}`
+  );
+
+  // Check that the decrement button is not disabled.
+  await assertAttributeValues(t, ex.dec.sel, 'aria-disabled', 'false');
+
+  // Check that the increment button is disabled.
+  await assertAttributeValues(t, ex.inc.sel, 'aria-disabled', 'true');
+});
+
+ariaTest('home', exampleFile, 'spinbutton-home', async (t) => {
+  const spinner = await t.context.session.findElement(By.css(ex.spin.sel));
+  const min = parseInt(ex.spin.min);
+
+  // Send home key
+  await spinner.sendKeys(Key.HOME);
+  t.is(
+    parseInt(await spinner.getAttribute('aria-valuenow')),
+    min,
+    `After sending end key, aria-valuenow should be the minimum value: ${min}`
+  );
+
+  // Check that the decrement button is disabled.
+  await assertAttributeValues(t, ex.dec.sel, 'aria-disabled', 'true');
+
+  // Check that the increment button is not disabled.
+  await assertAttributeValues(t, ex.inc.sel, 'aria-disabled', 'false');
+});
+
+ariaTest('up arrow', exampleFile, 'spinbutton-up-arrow', async (t) => {
+  const spinner = await t.context.session.findElement(By.css(ex.spin.sel));
+  const min = parseInt(ex.spin.min);
+  const max = parseInt(ex.spin.max);
+  let val = min;
+
+  // Send home key
+  await spinner.sendKeys(Key.HOME);
+
+  // Send arrow keys up to and including the maximum value.
+  while (++val <= max) {
+    await spinner.sendKeys(Key.ARROW_UP);
+
+    // Check that aria-valuenow is updated correctly.
+    t.is(
+      parseInt(await spinner.getAttribute('aria-valuenow')),
+      val,
+      `After sending ${val - 1} up arrows, aria-valuenow should be ${val}`
+    );
+  }
+
+  // Check that the decrement button is no longer disabled.
+  await assertAttributeValues(t, ex.dec.sel, 'aria-disabled', 'false');
+
+  // Check that the increment button is now disabled.
+  await assertAttributeValues(t, ex.inc.sel, 'aria-disabled', 'true');
+
+  // Send one more and check that aria-valuenow remains at the maximum value.
+  await spinner.sendKeys(Key.ARROW_UP);
+  t.is(
+    parseInt(await spinner.getAttribute('aria-valuenow')),
+    max,
+    `After sending one more up arrow, aria-valuenow should still be ${max}`
+  );
+
+  // Check that the decrement button is still not disabled
+  await assertAttributeValues(t, ex.dec.sel, 'aria-disabled', 'false');
+
+  // Check that the increment button is still disabled
+  await assertAttributeValues(t, ex.inc.sel, 'aria-disabled', 'true');
+});
+
+ariaTest('down arrow', exampleFile, 'spinbutton-down-arrow', async (t) => {
+  const spinner = await t.context.session.findElement(By.css(ex.spin.sel));
+  const min = parseInt(ex.spin.min);
+  const max = parseInt(ex.spin.max);
+  let val = max;
+
+  // Send end key
+  await spinner.sendKeys(Key.END);
+
+  // Send arrow keys down to and including the minimum value.
+  while (--val >= min) {
+    await spinner.sendKeys(Key.ARROW_DOWN);
+
+    // Check that aria-valuenow is updated correctly.
+    t.is(
+      parseInt(await spinner.getAttribute('aria-valuenow')),
+      val,
+      `After sending ${val + 1} down arrows, aria-valuenow should be ${val}`
+    );
+  }
+
+  // Check that the decrement button is now disabled.
+  await assertAttributeValues(t, ex.dec.sel, 'aria-disabled', 'true');
+
+  // Check that the increment button is no longer disabled.
+  await assertAttributeValues(t, ex.inc.sel, 'aria-disabled', 'false');
+
+  // Send one more and check that aria-valuenow remains at the maximum value.
+  await spinner.sendKeys(Key.ARROW_DOWN);
+  t.is(
+    parseInt(await spinner.getAttribute('aria-valuenow')),
+    min,
+    `After sending one more up arrow, aria-valuenow should still be ${min}`
+  );
+
+  // Check that the decrement button is still disabled.
+  await assertAttributeValues(t, ex.dec.sel, 'aria-disabled', 'true');
+
+  // Check that the increment button is still not disabled.
+  await assertAttributeValues(t, ex.inc.sel, 'aria-disabled', 'false');
+});
+
+// text input
+
+ariaTest(
+  'Expected behavior for direct input of numeric values',
+  exampleFile,
+  'standard-single-line-editing-keys',
+  async (t) => {
+    const spinner = await t.context.session.findElement(By.css(ex.spin.sel));
+    const min = parseInt(ex.spin.min);
+    const max = parseInt(ex.spin.max);
+
+    for (const inputScenario of Object.entries(ex.inputScenarios)) {
+      const [input, expected] = inputScenario;
+
+      // Input the value
+      await spinner.clear();
+      await spinner.sendKeys(Key.chord(Key.COMMAND, 'a'));
+      await spinner.sendKeys(input);
+
+      // Force blur after input
+      await t.context.session.executeScript((el) => el.blur(), spinner);
+
+      // Check that aria-valuenow is updated correctly.
+      t.is(
+        parseInt(await spinner.getAttribute('aria-valuenow')),
+        parseInt(expected),
+        `After inputting “${input}”, aria-valuenow should be ${expected}`
+      );
+
+      // Check that the decrement button has the expected aria-disabled state.
+      await assertAttributeValues(
+        t,
+        ex.dec.sel,
+        'aria-disabled',
+        parseInt(expected) <= parseInt(min) ? 'true' : 'false'
+      );
+
+      // Check that the increment button has the expected aria-disabled state.
+      await assertAttributeValues(
+        t,
+        ex.inc.sel,
+        'aria-disabled',
+        parseInt(expected) >= parseInt(max) ? 'true' : 'false'
+      );
+    }
+  }
+);
+
+// incerment and decrement buttons
+
+ariaTest('increment button', exampleFile, 'increment-button', async (t) => {
+  const spinner = await t.context.session.findElement(By.css(ex.spin.sel));
+  const status = await t.context.session.findElement(By.css(ex.status.sel));
+  const button = await t.context.session.findElement(By.css(ex.inc.sel));
+  const min = parseInt(ex.spin.min);
+  const max = parseInt(ex.spin.max);
+  let val = min;
+
+  // Send home key
+  await spinner.sendKeys(Key.HOME);
+
+  // Click increment button up to and including the maximum value.
+  while (++val <= max) {
+    await button.click();
+
+    // Check that aria-valuenow is updated correctly.
+    t.is(
+      parseInt(await spinner.getAttribute('aria-valuenow')),
+      val,
+      `After clicking ${val - 1} times, aria-valuenow should be ${val}`
+    );
+
+    // Check that the status element has the expected value.
+    t.is(
+      await status.getText(),
+      String(val),
+      `After clicking ${val - 1} times, status should be ${val}`
+    );
+  }
+
+  // Check that the decrement button is no longer disabled.
+  await assertAttributeValues(t, ex.dec.sel, 'aria-disabled', 'false');
+
+  // Check that the increment button is now disabled.
+  await assertAttributeValues(t, ex.inc.sel, 'aria-disabled', 'true');
+
+  // Send one more and check that aria-valuenow remains at the maximum value.
+  await button.click();
+  t.is(
+    parseInt(await spinner.getAttribute('aria-valuenow')),
+    max,
+    `After clicking once more, aria-valuenow should still be ${max}`
+  );
+
+  // Check that the status element has the expected value.
+  t.is(
+    await status.getText(),
+    String(max),
+    `After clicking once more, status should still be ${max}`
+  );
+
+  // Check that the decrement button is still not disabled
+  await assertAttributeValues(t, ex.dec.sel, 'aria-disabled', 'false');
+
+  // Check that the increment button is still disabled
+  await assertAttributeValues(t, ex.inc.sel, 'aria-disabled', 'true');
+
+  // Wait for the status to self-destruct
+  await t.context.session.sleep(parseInt(ex.status.selfDestruct) + 500);
+
+  // Check that the status element is empty
+  t.is(
+    await status.getText(),
+    '',
+    `After waiting for the status self-destruct timer, it should be empty`
+  );
+});
+
+ariaTest('decrement button', exampleFile, 'decrement-button', async (t) => {
+  const spinner = await t.context.session.findElement(By.css(ex.spin.sel));
+  const status = await t.context.session.findElement(By.css(ex.status.sel));
+  const button = await t.context.session.findElement(By.css(ex.dec.sel));
+  const min = parseInt(ex.spin.min);
+  const max = parseInt(ex.spin.max);
+  let val = max;
+
+  // Send end key
+  await spinner.sendKeys(Key.END);
+
+  // Click decrement button down to and including the minimum value.
+  while (--val >= min) {
+    await button.click();
+
+    // Check that aria-valuenow is updated correctly.
+    t.is(
+      parseInt(await spinner.getAttribute('aria-valuenow')),
+      val,
+      `After clicking ${val + 1} times, aria-valuenow should be ${val}`
+    );
+
+    // Check that the status element has the expected value.
+    t.is(
+      await status.getText(),
+      String(val),
+      `After clicking ${val + 1} times, status should be ${val}`
+    );
+  }
+
+  // Check that the decrement button is now disabled.
+  await assertAttributeValues(t, ex.dec.sel, 'aria-disabled', 'true');
+
+  // Check that the increment button is no longer disabled.
+  await assertAttributeValues(t, ex.inc.sel, 'aria-disabled', 'false');
+
+  // Send one more and check that aria-valuenow remains at the maximum value.
+  await button.click();
+  t.is(
+    parseInt(await spinner.getAttribute('aria-valuenow')),
+    min,
+    `After clicking once more, aria-valuenow should still be ${min}`
+  );
+
+  // Check that the status element has the expected value.
+  t.is(
+    await status.getText(),
+    String(min),
+    `After clicking once more, status should still be ${min}`
+  );
+
+  // Check that the decrement button is still disabled
+  await assertAttributeValues(t, ex.dec.sel, 'aria-disabled', 'true');
+
+  // Check that the increment button is still not disabled
+  await assertAttributeValues(t, ex.inc.sel, 'aria-disabled', 'false');
+
+  // Wait for the status to self-destruct
+  await t.context.session.sleep(parseInt(ex.status.selfDestruct) + 500);
+
+  // Check that the status element is empty
+  t.is(
+    await status.getText(),
+    '',
+    `After waiting for the status self-destruct timer, it should be empty`
+  );
+});
