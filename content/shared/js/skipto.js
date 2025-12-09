e4f55e3deb11e6c03394379a312dff7035fcfda1
Jon Gunderson <jongund@illinois.edu>
2025-12-09 13:49:17 -0600

    fixed issue with about dialog title

--- content/shared/js/skipto.js
+++ content/shared/js/skipto.js
@@ -1509,106 +1509,116 @@ dialog button:hover {
   const templateInfoDialog = document.createElement('template');
   templateInfoDialog.innerHTML = `
   <dialog id="${DIALOG_ID}">
-    <div class="header">
-      <h2>Keyboard Shortcuts</h2>
-      <button>✕</button>
+
+    <div class="shortcuts">
+      <div class="header">
+        <h2>Keyboard Shortcuts</h2>
+        <button>✕</button>
+      </div>
+
+      <div class="content">
+         <table>
+            <caption>Landmark Regions</caption>
+            <thead>
+               <tr>
+                  <th class="shortcut">Key</th>
+                  <th class="desc">Description</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td class="shortcut">r</td>
+                  <td class="desc">Next region</td>
+               </tr>
+               <tr>
+                  <td class="shortcut">R</td>
+                  <td class="desc">Previous region</td>
+               </tr>
+               <tr>
+                  <td class="shortcut">m</td>
+                  <td class="desc">Main regions</td>
+               </tr>
+               <tr>
+                  <td class="shortcut">n</td>
+                  <td class="desc">Navigation regions</td>
+               </tr>
+               <tr>
+                  <td class="shortcut">c</td>
+                  <td class="desc">Complementary regions</td>
+               </tr>
+            </tbody>
+         </table>
+         <table>
+            <caption>Headings</caption>
+            <thead>
+               <tr>
+                  <th class="shortcut">Key</th>
+                  <th class="desc">Description</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td class="shortcut">h</td>
+                  <td class="desc">Next heading</td>
+               </tr>
+               <tr>
+                  <td class="shortcut">H</td>
+                  <td class="desc">Previous heading</td>
+               </tr>
+               <tr>
+                  <td class="shortcut">1</td>
+                  <td class="desc">Level 1 headings</td>
+               </tr>
+               <tr>
+                  <td class="shortcut">2</td>
+                  <td class="desc">Level 2 headings</td>
+               </tr>
+               <tr>
+                  <td class="shortcut">3</td>
+                  <td class="desc">Level 3 headings</td>
+               </tr>
+               <tr>
+                  <td class="shortcut">4</td>
+                  <td class="desc">Level 4 headings</td>
+               </tr>
+               <tr>
+                  <td class="shortcut">5</td>
+                  <td class="desc">Level 5 headings</td>
+               </tr>
+               <tr>
+                  <td class="shortcut">6</td>
+                  <td class="desc">Level 6 headings</td>
+               </tr>
+            </tbody>
+         </table>
+      </div>
     </div>
 
-    <div class="content shortcuts">
-       <table>
-          <caption>Landmark Regions</caption>
-          <thead>
-             <tr>
-                <th class="shortcut">Key</th>
-                <th class="desc">Description</th>
-             </tr>
-          </thead>
-          <tbody>
-             <tr>
-                <td class="shortcut">r</td>
-                <td class="desc">Next region</td>
-             </tr>
-             <tr>
-                <td class="shortcut">R</td>
-                <td class="desc">Previous region</td>
-             </tr>
-             <tr>
-                <td class="shortcut">m</td>
-                <td class="desc">Main regions</td>
-             </tr>
-             <tr>
-                <td class="shortcut">n</td>
-                <td class="desc">Navigation regions</td>
-             </tr>
-             <tr>
-                <td class="shortcut">c</td>
-                <td class="desc">Complementary regions</td>
-             </tr>
-          </tbody>
-       </table>
-       <table>
-          <caption>Headings</caption>
-          <thead>
-             <tr>
-                <th class="shortcut">Key</th>
-                <th class="desc">Description</th>
-             </tr>
-          </thead>
-          <tbody>
-             <tr>
-                <td class="shortcut">h</td>
-                <td class="desc">Next heading</td>
-             </tr>
-             <tr>
-                <td class="shortcut">H</td>
-                <td class="desc">Previous heading</td>
-             </tr>
-             <tr>
-                <td class="shortcut">1</td>
-                <td class="desc">Level 1 headings</td>
-             </tr>
-             <tr>
-                <td class="shortcut">2</td>
-                <td class="desc">Level 2 headings</td>
-             </tr>
-             <tr>
-                <td class="shortcut">3</td>
-                <td class="desc">Level 3 headings</td>
-             </tr>
-             <tr>
-                <td class="shortcut">4</td>
-                <td class="desc">Level 4 headings</td>
-             </tr>
-             <tr>
-                <td class="shortcut">5</td>
-                <td class="desc">Level 5 headings</td>
-             </tr>
-             <tr>
-                <td class="shortcut">6</td>
-                <td class="desc">Level 6 headings</td>
-             </tr>
-          </tbody>
-       </table>
-    </div>
 
-    <div class="content about">
-      <div class="desc">
-        SkipTo.js is a free and open source utility to support the WCAG 2.4.1 Bypass Block requirement.
+    <div class="about">
+      <div class="header">
+        <h2>About SkipTo.js</h2>
+        <button>✕</button>
       </div>
-      <div class="privacy-label">
-        Privacy
-      </div>
-      <div class="privacy">
-        SkipTo.js does not collect or store any information about users or work with any other parties to collect or share user browsing information.
-      </div>
-      <div class="happy">
-        Happy Skipping!
-      </div>
-      <div class="version">
-        Version ${VERSION}
-      </div>
-      <div class="copyright">
-        BSD License, Copyright 2021-2025
+      <div class="content">
+        <div class="desc">
+          SkipTo.js is a free and open source utility to support the WCAG 2.4.1 Bypass Block requirement.
+        </div>
+        <div class="privacy-label">
+          Privacy
+        </div>
+        <div class="privacy">
+          SkipTo.js does not collect or store any information about users or work with any other parties to collect or share user browsing information.
+        </div>
+        <div class="happy">
+          Happy Skipping!
+        </div>
+        <div class="version">
+          Version ${VERSION}
+        </div>
+        <div class="copyright">
+          BSD License, Copyright 2021-2025
+        </div>
       </div>
     </div>
 
@@ -1642,8 +1652,8 @@ dialog button:hover {
       this.closeButtonElem1.addEventListener('click', this.onCloseButtonClick.bind(this));
       this.closeButtonElem1.addEventListener('keydown', this.onKeyDown.bind(this));
 
-      this.shortcutContentElem = attachElem.querySelector('.content.shortcuts');
-      this.aboutContentElem    = attachElem.querySelector('.content.about');
+      this.shortcutContentElem = attachElem.querySelector('.shortcuts');
+      this.aboutContentElem    = attachElem.querySelector('.about');
 
       const moreInfoButtonElem = attachElem.querySelector('.buttons button.more');
       moreInfoButtonElem.addEventListener('click', this.onMoreInfoClick.bind(this));
